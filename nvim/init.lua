d3541b4cffb5ca6884609468ee6caab656459379
Stefan Genov <sgenov@vmware.com>
2023-08-15 17:41:51 +0300

    [master] Progress

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,4 +16,5 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
+
 require('stef')
--- nvim/lua/stef/plugins.lua
+++ nvim/lua/stef/plugins.lua
@@ -1,3 +1,22 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+require('packer').startup(function(use)
+end)
+
+
+
+
 return require('packer').startup(function(use)
   -- Configurations will go here soon
 	use 'wbthomason/packer.nvim'
@@ -47,8 +66,12 @@ return require('packer').startup(function(use)
 		{'hrsh7th/nvim-cmp'},     -- Required
 		{'hrsh7th/cmp-nvim-lsp'}, -- Required
 		{'L3MON4D3/LuaSnip'},     -- Required
+		}
 	}
-}
+
+	 if packer_bootstrap then
+	    require('packer').sync()
+	 end
 end)
 
 
