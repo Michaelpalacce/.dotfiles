476608d513e04414ef2ec6e853a724968d13a74c
Stefan Genov <sgenov@vmware.com>
2023-08-17 18:38:27 +0300

    [master] Updates

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,12 +1,13 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-
-
 vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Add File To Harpoon" })
 vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu, { desc = "Show Harpoon Menu" })
-vim.keymap.set("n", "<leader>1", function() ui.nav_prev() end)
-vim.keymap.set("n", "<leader>2", function() ui.nav_next() end)
-
+vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end, { desc = "Harpoon: 1" })
+vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end, { desc = "Harpoon: 2" })
+vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end, { desc = "Harpoon: 3" })
+vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end, { desc = "Harpoon: 4" })
+vim.keymap.set("n", "<leader>p", function() ui.nav_prev() end, { desc = "Harpoon: Go To Next" })
+vim.keymap.set("n", "<leader>n", function() ui.nav_next() end, { desc = "Harpoon: Go To Previous" })
 
 
--- nvim/lua/stef/remap.lua
+++ nvim/lua/stef/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>fe", vim.cmd.Ex) -- <SPACE> fe -- stands for file explore
-local expr_opts = { noremap = true, expr = true, silent = true }
+
+vim.keymap.set("n", "Q", "<nop>")
 
 
