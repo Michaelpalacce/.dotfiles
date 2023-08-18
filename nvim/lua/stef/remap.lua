c6eb047dd23c27cee501e7676728584c93aa4395
Stefan Genov <sgenov@vmware.com>
2023-08-18 10:27:09 +0300

    Added Remaps to remove q (recording) and added a custom shortcut to duplicate line

--- nvim/lua/stef/remap.lua
+++ nvim/lua/stef/remap.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>fe", vim.cmd.Ex) -- <SPACE> fe -- stands for file explore
+-- vim.keymap.set("n", "<leader>fe", vim.cmd.Ex) -- <SPACE> fe -- stands for file explore
 
 vim.keymap.set("n", "Q", "<nop>")
+vim.keymap.set("n", "q", "<nop>", { desc = 'Remove recording feature' })
 
-
+vim.keymap.set('n', "<C-d>", "VYp", { desc = 'Dupicate current line' })
