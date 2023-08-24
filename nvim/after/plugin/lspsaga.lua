151a4e271294e010cd325231efef3c96d426b643
Stefan Genov <stefantigro@gmail.com>
2023-08-25 00:54:00 +0300

    Update lspsaga.lua

--- nvim/after/plugin/lspsaga.lua
+++ nvim/after/plugin/lspsaga.lua
@@ -7,7 +7,6 @@ require('lspsaga').setup({
 vim.keymap.set({ 'n', 'v' }, '<leader>lo', '<cmd>Lspsaga outline<CR>', { desc = 'Open LSP Saga Outline' })
 vim.keymap.set('n', '<F2>', '<cmd>Lspsaga rename<CR>', { desc = 'Rename struct under cursor' })
 vim.keymap.set('n', '<c-space>', '<cmd>Lspsaga code_action<CR>', { desc= 'Trigger code actions' })
-vim.keymap.set('n', '<leader>lp', '<cmd>Lspsaga peek_definition<CR>', { desc= 'Peek the definition of the struct under cursor' })
 
 -- TODO: STILL NEEDS WORK
 -- https://nvimdev.github.io/lspsaga/diagnostic/
