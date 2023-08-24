f1363cb6cd59295fd55c3d25fdcc7463196592db
Stefan Genov <stefantigro@gmail.com>
2023-08-25 00:52:01 +0300

    Update harpoon.lua

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,8 +1,8 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Add File To Harpoon" })
-vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu, { desc = "Show Harpoon Menu" })
+vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "[A]dd File To Harpoon" })
+vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu, { desc = "Show [H]arpoon Menu" })
 vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end, { desc = "Harpoon: 1" })
 vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end, { desc = "Harpoon: 2" })
 vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end, { desc = "Harpoon: 3" })
@@ -13,7 +13,7 @@ vim.keymap.set("n", "<leader>7", function() ui.nav_file(7) end, { desc = "Harpoo
 vim.keymap.set("n", "<leader>8", function() ui.nav_file(8) end, { desc = "Harpoon: 8" })
 vim.keymap.set("n", "<leader>9", function() ui.nav_file(9) end, { desc = "Harpoon: 9" })
 vim.keymap.set("n", "<leader>0", function() ui.nav_file(0) end, { desc = "Harpoon: 0" })
-vim.keymap.set("n", "<leader>p", function() ui.nav_prev() end, { desc = "Harpoon: Go To Next" })
-vim.keymap.set("n", "<leader>n", function() ui.nav_next() end, { desc = "Harpoon: Go To Previous" })
+vim.keymap.set("n", "<leader>p", function() ui.nav_prev() end, { desc = "Harpoon: Go To Previous" })
+vim.keymap.set("n", "<leader>n", function() ui.nav_next() end, { desc = "Harpoon: Go To Next" })
 
 
