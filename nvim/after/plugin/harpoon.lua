b4f3f1dd59617307932d2d6ee05e6ad8fb6f3b8f
Stefan Genov <sgenov@vmware.com>
2023-08-24 18:12:49 +0300

    [master] Harpoon more bindaings

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -7,6 +7,12 @@ vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end, { desc = "Harpoo
 vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end, { desc = "Harpoon: 2" })
 vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end, { desc = "Harpoon: 3" })
 vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end, { desc = "Harpoon: 4" })
+vim.keymap.set("n", "<leader>5", function() ui.nav_file(5) end, { desc = "Harpoon: 5" })
+vim.keymap.set("n", "<leader>6", function() ui.nav_file(6) end, { desc = "Harpoon: 6" })
+vim.keymap.set("n", "<leader>7", function() ui.nav_file(7) end, { desc = "Harpoon: 7" })
+vim.keymap.set("n", "<leader>8", function() ui.nav_file(8) end, { desc = "Harpoon: 8" })
+vim.keymap.set("n", "<leader>9", function() ui.nav_file(9) end, { desc = "Harpoon: 9" })
+vim.keymap.set("n", "<leader>0", function() ui.nav_file(0) end, { desc = "Harpoon: 0" })
 vim.keymap.set("n", "<leader>p", function() ui.nav_prev() end, { desc = "Harpoon: Go To Next" })
 vim.keymap.set("n", "<leader>n", function() ui.nav_next() end, { desc = "Harpoon: Go To Previous" })
 
