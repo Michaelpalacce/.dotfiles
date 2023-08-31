4d10c7c0c3ce7fd5dc5286f0192ca35eed894630
Stefan Genov <sgenov@vmware.com>
2023-08-31 17:02:14 +0300

    feat: Rolled back to dracula.... not sure why my editor puts d's and other characters slanted in some cases and they get cut off

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,13 +1,13 @@
 -- Catppuccin
 -- vim.cmd.colorscheme('catppuccin-latte')
-vim.cmd.colorscheme('catppuccin-mocha')
+-- vim.cmd.colorscheme('catppuccin-mocha')
 -- vim.cmd.colorscheme('catppuccin-frappe')
 -- vim.cmd.colorscheme('catppuccin-macchiato')
 
 -- Dracula
 
 -- vim.cmd.colorscheme('dracula')
--- vim.cmd.colorscheme('dracula-soft')
+vim.cmd.colorscheme('dracula-soft')
 
 -- Rose-pine
 
