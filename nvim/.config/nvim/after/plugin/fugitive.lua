c01053f68411962a4bab0ed8fb7320248531c350
Stefan Genov <sgenov@vmware.com>
2023-09-01 13:59:08 +0300

    feat: Added better git commands to help me out thanks to telescope and vim-fugitive

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -1,28 +1,28 @@
-vim.keymap.set('n', "<leader>tg", vim.cmd.Git, { desc = '[T]oggle [G]it Status' })
-local stef_Fugitive = vim.api.nvim_create_augroup("stef_Fugitive", {})
+local builtin = require('telescope.builtin')
+-- Yes we depend on telescope boiiiiiiiiiiiiiiiiiii. Yes it is hidden
 
+vim.keymap.set('n', "<leader>gs", vim.cmd.Git, { desc = '[T]oggle [G]it Status' })
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = 'Telescope: [G]it [B]ranches' })
+
+local stef_Fugitive = vim.api.nvim_create_augroup("stef_Fugitive", {})
 local autocmd = vim.api.nvim_create_autocmd
 autocmd("BufWinEnter", {
-    group = stef_Fugitive,
-    pattern = "*",
-    callback = function()
-        if vim.bo.ft ~= "fugitive" then
-            return
-        end
+	group = stef_Fugitive,
+	pattern = "*",
+	callback = function()
+		if vim.bo.ft ~= "fugitive" then
+			return
+		end
 
-        local bufnr = vim.api.nvim_get_current_buf()
-        local opts = { buffer = bufnr, remap = false }
-        vim.keymap.set("n", "<leader>p", function()
-            vim.cmd.Git('push')
-        end, opts)
+		local bufnr = vim.api.nvim_get_current_buf()
 
-        -- rebase always
-        vim.keymap.set("n", "<leader>P", function()
-            vim.cmd.Git({ 'pull', '--rebase' })
-        end, opts)
+		vim.keymap.set('n', "<leader>gp", function()
+			vim.cmd.Git('push')
+		end, { desc = "Fugitive: [G]it [P]ush", buffer = bufnr, remap = false })
 
-        -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-        -- needed if i did not set the branch up correctly
-        vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
-    end,
+		-- rebase always
+		vim.keymap.set("n", "<leader>gP", function()
+			vim.cmd.Git({ 'pull', '--rebase' })
+		end, { buffer = bufnr, remap = false, desc = "Fugitive: [G]it [P]ull" })
+	end,
 })
--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -2,10 +2,14 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope: [F]ind [F]iles' })
 vim.keymap.set('n', '<leader>fr', builtin.resume, { desc = 'Telescope: [F]inder [R]esume' })
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, { desc = 'Telescope: [F]ind Word in Project [S]cope (live grep)' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope: [F]ind in [B]uffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope: [F]ind [H]elp Tags' })
 vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Telescope: [F]ind [G]it files' })
 vim.keymap.set('n', '<leader>ft', builtin.colorscheme, { desc = 'Telescope: [F]ind and Select [T]heme (colorscheme)' })
+vim.keymap.set('n', '<leader>fq', builtin.quickfix, { desc = 'Telescope: [F]ind In The [Q]uickfix List' })
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Telescope: [F]ind [K]eymaps' })
+vim.keymap.set('n', '<leader>fo', builtin.vim_options, { desc = 'Telescope: [F]ind Vim [O]ptions' })
+
+vim.keymap.set('n', '<leader>fb', builtin.git_branches, { desc = 'Telescope: [F]ind Git [B]ranches' })
 
 require('telescope').setup {
 	defaults = {
