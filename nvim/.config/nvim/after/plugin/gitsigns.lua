982477672cd5484b7cbd2371c58a36ce6450a79a
Stefan Genov <sgenov@vmware.com>
2023-09-05 14:24:17 +0300

    feat: Configured git signs

--- nvim/.config/nvim/after/plugin/gitsigns.lua
+++ nvim/.config/nvim/after/plugin/gitsigns.lua
@@ -1,3 +1,13 @@
 require('gitsigns').setup {
-	current_line_blame = true
+	current_line_blame      = true,
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+		delay = 500,
+		ignore_whitespace = false,
+	},
+	signcolumn              = true, -- Toggle with `:Gitsigns toggle_signs`
+	numhl                   = true, -- Toggle with `:Gitsigns toggle_numhl`
+	linehl                  = false, -- Toggle with `:Gitsigns toggle_linehl`
+	word_diff               = false, -- Toggle with `:Gitsigns toggle_word_diff`
 }
