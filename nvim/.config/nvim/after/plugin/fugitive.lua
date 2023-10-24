4dfd16fe5023abd4e94c6a5180ae2ff7bce21795
Stefan Genov <sgenov@vmware.com>
2023-10-24 19:01:55 +0300

    feat: Git commands now no longer need to be executed in the status window

--- nvim/.config/nvim/after/plugin/fugitive.lua
+++ nvim/.config/nvim/after/plugin/fugitive.lua
@@ -5,24 +5,10 @@ vim.keymap.set('n', "<leader>gs", vim.cmd.Git, { desc = '[T]oggle [G]it Status' 
 vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = 'Telescope: [G]it [B]ranches' })
 vim.keymap.set('n', '<leader>gd', builtin.git_status, { desc = 'Telescope: [G]it [D]iff' })
 
-local stef_Fugitive = vim.api.nvim_create_augroup("stef_Fugitive", {})
-local autocmd = vim.api.nvim_create_autocmd
-autocmd("BufWinEnter", {
-	group = stef_Fugitive,
-	pattern = "*",
-	callback = function()
-		if vim.bo.ft ~= "fugitive" then
-			return
-		end
+vim.keymap.set('n', "<leader>gp", function()
+	vim.cmd.Git('push')
+end, { desc = "Fugitive: [G]it [P]ush", remap = false })
 
-		local bufnr = vim.api.nvim_get_current_buf()
-
-		vim.keymap.set('n', "<leader>gp", function()
-			vim.cmd.Git('push')
-		end, { desc = "Fugitive: [G]it [P]ush", buffer = bufnr, remap = false })
-
-		vim.keymap.set("n", "<leader>gP", function()
-			vim.cmd.Git('pull')
-		end, { buffer = bufnr, remap = false, desc = "Fugitive: [G]it [P]ull" })
-	end,
-})
+vim.keymap.set("n", "<leader>gP", function()
+	vim.cmd.Git('pull')
+end, { remap = false, desc = "Fugitive: [G]it [P]ull" })
