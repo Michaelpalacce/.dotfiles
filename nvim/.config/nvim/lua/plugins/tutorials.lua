d3a48c6417f30bd9d46c158ae87f0a4c3faa2b71
Stefan Genov <stefantigro@gmail.com>
2023-11-07 16:02:52 +0200

    feat: fixx

--- nvim/.config/nvim/lua/plugins/tutorials.lua
+++ nvim/.config/nvim/lua/plugins/tutorials.lua
@@ -1,6 +1,18 @@
 return {
 
-	{ 'folke/which-key.nvim' }, -- Show tooltips what key combinations you can do
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		}
+	},
 	{
 		'sudormrfbin/cheatsheet.nvim',
 		dependencies = {
