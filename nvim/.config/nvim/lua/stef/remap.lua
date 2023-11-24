ab08c30de82656e3b5ac5192fd3c789626d5f15b
Stefan Genov <stefantigro@gmail.com>
2023-11-24 18:51:39 +0200

    feat: Now we can move with C-hjkl

--- nvim/.config/nvim/lua/stef/remap.lua
+++ nvim/.config/nvim/lua/stef/remap.lua
@@ -36,9 +36,6 @@ vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 -- vim.keymap.set("v", "<A-Down>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
 -- vim.keymap.set("v", "<A-Up>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
--- -- Reload
--- vim.keymap.set("n", "<leader>fr", ":e!<CR>", { desc = "[F]ile: [R]eload" })
-
 -- Move Between Windows
 -- vim.keymap.set('n', '<C-h>', '<C-w>h')
 -- vim.keymap.set('n', '<C-j>', '<C-w>j')
@@ -50,9 +47,15 @@ vim.keymap.set('n', '<C-Up>', '<C-w>k')
 vim.keymap.set('n', '<C-Right>', '<C-w>l')
 vim.keymap.set('n', '<C-q>', '<C-w>q')
 
+-- Stop inserting and move cursor, re-enter insert
+vim.keymap.set({ "n", "i", "v" }, '<C-h>', '<Left>', { desc = "Move cursor left while editing" })
+vim.keymap.set({ "n", "i", "v" }, '<C-l>', '<Right>', { desc = "Move cursor right while editing" })
+vim.keymap.set({ "n", "i", "v" }, '<C-j>', '<Down>', { desc = "Move cursor down while editing" })
+vim.keymap.set({ "n", "i", "v" }, '<C-k>', '<Up>', { desc = "Move cursor up while editing" })
+
 -- Quick Fix
-vim.keymap.set("n", "<A-]>", "<cmd>cnext<CR>zz", { desc = 'Go to the next quickfix item' })
-vim.keymap.set("n", "<A-[>", "<cmd>cprev<CR>zz", { desc = 'Go to the previous quickfix item' })
+vim.keymap.set("n", "<C-]>", "<cmd>cnext<CR>zz", { desc = 'Go to the next quickfix item' })
+vim.keymap.set("n", "<C-[>", "<cmd>cprev<CR>zz", { desc = 'Go to the previous quickfix item' })
 
 -- Buffers
 vim.keymap.set("n", "<leader>bD", "<cmd>%bd|e#<CR>", { desc = '[B]uffers [D]elete all but current' })
