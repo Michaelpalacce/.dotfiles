143aeab17cf8a4189739130eee03e43d002673d2
Stefan Genov <stefantigro@gmail.com>
2023-12-08 08:15:20 +0200

    feat: Returned chatgpt, let it stay in the background, furthermore removed enter for code accepting as a mapping

--- nvim/.config/nvim/lua/plugins/configs/cmp.lua
+++ nvim/.config/nvim/lua/plugins/configs/cmp.lua
@@ -11,7 +11,7 @@ cmp.setup({
 	},
 	mapping = {
 		-- `Enter` key to confirm completion
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
+		-- ['<CR>'] = cmp.mapping.confirm({ select = true }),
 
 		-- Ctrl+Space to trigger completion menu
 		['<C-Space>'] = cmp.mapping.complete(),
