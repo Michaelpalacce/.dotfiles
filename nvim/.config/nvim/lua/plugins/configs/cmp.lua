e038257cfbd3cb8029262d1e0e98d787ee601ca2
Stefan Genov <sgenov@sgenov7HCQN.vmware.com>
2023-12-09 11:42:35 +0200

    feat: different lspkind mode

--- nvim/.config/nvim/lua/plugins/configs/cmp.lua
+++ nvim/.config/nvim/lua/plugins/configs/cmp.lua
@@ -67,7 +67,7 @@ cmp.setup({
 	formatting = {
 		fields = { 'abbr', 'kind', 'menu' },
 		format = require('lspkind').cmp_format({
-			mode = 'symbol', -- show only symbol annotations
+			mode = 'symbol_text', -- show only symbol annotations
 			maxwidth = 50, -- prevent the popup from showing more than provided characters
 			ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
 		})
