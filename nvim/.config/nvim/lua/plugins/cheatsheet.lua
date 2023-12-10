5191a73e882a870c9cdfd2ba57760ec5411d01f3
Stefan Genov <stefantigro@gmail.com>
2023-12-11 00:59:49 +0200

    feat: removed a few plugins that are no longer needed

--- nvim/.config/nvim/lua/plugins/cheatsheet.lua
+++ nvim/.config/nvim/lua/plugins/cheatsheet.lua
@@ -3,7 +3,7 @@ return {
 		'sudormrfbin/cheatsheet.nvim', -- Cheatsheet for nvim
 		dependencies = {
 			{ 'nvim-telescope/telescope.nvim' },
-			{ 'nvim-lua/popup.nvim' },
+			-- { 'nvim-lua/popup.nvim' }, -- Not needed, install if any issues arise?
 			{ 'nvim-lua/plenary.nvim' },
 		},
 		config = function()
--- nvim/.config/nvim/lua/plugins/obsession.lua
+++
@@ -1,4 +0,0 @@
-return {
-	-- Ability to save sessions. TBH I don't use it directly, using it through TMUX
-	{ 'tpope/vim-obsession' }
-}
