48c27d905b4b642d69304acaddab230c7793081e
Stefan Genov <stefan.genov@broadcom.com>
2023-12-15 14:05:55 +0200

    feat: added more keybinds to harpoon, essentially the ones I can reach

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -24,6 +24,12 @@ return {
 				{ desc = "[3] Select 3rd harpoon item" })
 			vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end,
 				{ desc = "[4] Select 4th harpoon item" })
+			vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end,
+				{ desc = "[5] Select 5th harpoon item" })
+			vim.keymap.set("n", "<leader>6", function() harpoon:list():select(6) end,
+				{ desc = "[6] Select 6th harpoon item" })
+			vim.keymap.set("n", "<leader>7", function() harpoon:list():select(7) end,
+				{ desc = "[7] Select 7th harpoon item" })
 		end
 
 	},
