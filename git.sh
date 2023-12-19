207dfc1b9ae059bbb496240e792963e84061ebc8
Stefan Genov <stefan.genov@broadcom.com>
2023-12-19 11:08:04 +0200

    feat: added a helper for git

---
+++ git.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git config --global user.name "Stefan Genov"
+git config --global user.email "stefantigro@gmail.com"
+git config --global core.editor "nvim"
+git config --global --add --bool push.autoSetupRemote true
