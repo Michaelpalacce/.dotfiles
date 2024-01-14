cba44a107d6bf49a492db89151e26404407898f5
Stefan Genov <stefan.genov@broadcom.com>
2024-01-14 19:56:35 +0200

    feat: Improvements to tmux-exxegutor tmux-sessionizer and fixed some remaps... we are going to switch sessions with tmux only now, not from neovim or bash

--- nvim/.config/nvim/lua/stef/remap.lua
+++ nvim/.config/nvim/lua/stef/remap.lua
@@ -22,9 +22,9 @@ end, { desc = "R[u]n TSUpdate, MasonUpdate, Lazy" })
 ------------------------------------------
 
 -- TMUX
-vim.keymap.set("n", "<leader>so", "<cmd>silent !tmux neww tmux-sessionizer 'nvim .'<CR>", { desc = "[S]cripts Tmux [S]essionizer With Nvim" })
+-- vim.keymap.set("n", "<leader>so", "<cmd>silent !tmux neww tmux-sessionizer 'nvim .'<CR>", { desc = "[S]cripts Tmux [S]essionizer With Nvim" })
 vim.keymap.set("n", "<leader>sc", "<cmd>silent !tmux neww tmux-cht<CR>", { desc = "[S]cripts [C]heatsheet" })
-vim.keymap.set("n", "<leader>sm", "<cmd>silent !tmux neww tmux-exxegutor<CR>", { desc = "[S]cripts [M]aven" })
+vim.keymap.set("n", "<leader>sb", "<cmd>silent !tmux neww tmux-exxegutor<CR>", { desc = "[S]cripts [M]aven" })
 vim.keymap.set("n", "<leader>si", "<cmd>silent !tmux neww tmux-identity-theft<CR>", { desc = "[S]cripts Switch [I]dentity" })
 
 -- Move Lines
