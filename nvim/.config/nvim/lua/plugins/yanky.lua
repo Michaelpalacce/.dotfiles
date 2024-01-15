23f4c43d6dcde84daeef04aa661107706a68a696
Stefan Genov <stefan.genov@broadcom.com>
2024-01-15 08:25:54 +0200

    feata: Yank "P" will now open the history

--- nvim/.config/nvim/lua/plugins/yanky.lua
+++ nvim/.config/nvim/lua/plugins/yanky.lua
@@ -9,13 +9,9 @@ return {
 			-- refer to the configuration section below
 		},
 		config = function()
-			vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
-			vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
-			vim.keymap.set("n", "<c-p>", "<Plug>(YankyPreviousEntry)")
-			vim.keymap.set("n", "<c-n>", "<Plug>(YankyNextEntry)")
-
 			require("telescope").load_extension("yank_history")
-			vim.keymap.set('n', "<leader>fy", ":Telescope yank_history<CR>", { desc = '[T]oggle [Y]ank History' })
+			vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+			vim.keymap.set({ "n", "x" }, "P", ":Telescope yank_history<CR>", { desc = '[T]oggle [Y]ank History' })
 
 			require("yanky").setup {
 				highlight = {
