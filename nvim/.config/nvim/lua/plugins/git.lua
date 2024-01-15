e9e905549124c2fe0e67667f6e1a76ffc68ee5f3
Stefan Genov <stefan.genov@broadcom.com>
2024-01-15 17:12:12 +0200

    feat: ]c and [c added for going forward and backwards changes

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -55,6 +55,19 @@ return {
 					vim.keymap.set(mode, l, r, opts)
 				end
 
+				-- Navigation
+				map('n', ']c', function()
+					if vim.wo.diff then return ']c' end
+					vim.schedule(function() gs.next_hunk() end)
+					return '<Ignore>'
+				end, { expr = true })
+
+				map('n', '[c', function()
+					if vim.wo.diff then return '[c' end
+					vim.schedule(function() gs.prev_hunk() end)
+					return '<Ignore>'
+				end, { expr = true })
+
 				-- Actions
 				map('n', '<leader>hs', gs.stage_hunk, { desc = '[H]unk [S]tage' })
 				map('n', '<leader>hr', gs.reset_hunk, { desc = '[H]unk [R]eset' })
