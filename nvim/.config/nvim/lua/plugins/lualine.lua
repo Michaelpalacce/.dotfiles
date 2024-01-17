536a41ad5d23041045eaedfcc702e24cc69eced4
Stefan Genov <stefan.genov@broadcom.com>
2024-01-17 22:15:46 +0200

    feat: Show full path for lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
 			sections = {
 				lualine_a = { 'mode' },
 				lualine_b = { 'branch', 'diff', 'diagnostics' },
-				lualine_c = { 'filename' },
+				lualine_c = { { 'filename', path = 1 } },
 				lualine_x = { 'filetype' },
 				lualine_y = {},
 				lualine_z = { 'location' }
