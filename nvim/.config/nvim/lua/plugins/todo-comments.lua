e79d118fe38e1cd06d085d50037d39d54c503609
Stefan Genov <stefan.genov@broadcom.com>
2024-01-17 09:34:36 +0200

    feat: Add `todo-comments` extra bindings and more configuration

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++ nvim/.config/nvim/lua/plugins/todo-comments.lua
@@ -4,7 +4,46 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			vim.keymap.set('n', "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = '[T]oggle [T]odo Comments' })
-			require("todo-comments").setup {}
+			vim.keymap.set("n", "]t", function()
+				require("todo-comments").jump_next()
+			end, { desc = "Next todo comment" })
+
+			vim.keymap.set("n", "[t", function()
+				require("todo-comments").jump_prev()
+			end, { desc = "Previous todo comment" })
+
+			require("todo-comments").setup {
+				signs = true, -- show icons in the signs column
+				sign_priority = 8, -- sign priority
+				-- keywords recognized as todo comments
+				keywords = {
+					FIX = {
+						icon = " ", -- icon used for the sign, and in search results
+						color = "error", -- can be a hex color, or a named color (see below)
+						alt = { "FIXME", "BUG", "FIXIT", "ISSUE" }, -- a set of other keywords that all map to this FIX keywords
+						-- signs = false, -- configure signs for some keywords individually
+					},
+					TODO = { icon = " ", color = "info", alt = { "todo" } },
+					HACK = { icon = " ", color = "warning" },
+					WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+					PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+					NOTE = { icon = " ", color = "hint", alt = { "INFO", "info" } },
+					TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED", "test" } },
+				},
+				search = {
+					command = "rg",
+					args = {
+						"--color=never",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+					},
+					-- regex that will be used to match keywords.
+					-- don't replace the (KEYWORDS) placeholder
+					pattern = [[\b(@KEYWORDS)]], -- ripgrep regex
+				},
+			}
 		end,
 		opts = {
 			-- your configuration comes here
