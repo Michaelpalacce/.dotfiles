3355033ef840cc14de6c25a4a4ad20c25c2d3704
Stefan Genov <stefan.genov@broadcom.com>
2024-01-21 00:08:25 +0200

    feat: added a new prompt and remapped text related keys

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,3 +1,6 @@
+local documentationPrompt =
+"Please add documentation to the following code adhering to the standard for the language. Please keep it short and to the point so it's easier to read. Remember to only add information that is not apparent form the method signature."
+
 return {
 	-- RUN :UpdateRemotePlugins
 	-- Dependencies: pip3 install python-dotenv requests pynvim prompt-toolkit
@@ -11,6 +14,9 @@ return {
 				Review = "Please review the following code and provide suggestions for improvement.",
 				Tests = "Please explain how the selected code works, then generate unit tests for it.",
 				Refactor = "Please refactor the following code to improve its clarity and readability.",
+				Documentation = documentationPrompt,
+				Simplify = "Please simplify the following code to make it easier to understand.",
+
 				-- Text related prompts
 				Summarize = "Please summarize the following text.",
 				Spelling = "Please correct any grammar and spelling errors in the following text.",
@@ -26,14 +32,19 @@ return {
 		end,
 		event = "VeryLazy",
 		keys = {
-			{ "<leader>cce", "<cmd>CopilotChatExplain<cr>",   desc = "CopilotChat - Explain code" },
-			{ "<leader>cct", "<cmd>CopilotChatTests<cr>",     desc = "CopilotChat - Generate tests" },
-			{ "<leader>ccr", "<cmd>CopilotChatReview<cr>",    desc = "CopilotChat - Review code" },
-			{ "<leader>ccR", "<cmd>CopilotChatRefactor<cr>",  desc = "CopilotChat - Refactor code" },
-			{ "<leader>ccs", "<cmd>CopilotChatSummarize<cr>", desc = "CopilotChat - Summarize text" },
-			{ "<leader>ccS", "<cmd>CopilotChatSpelling<cr>",  desc = "CopilotChat - Correct spelling" },
-			{ "<leader>ccw", "<cmd>CopilotChatWording<cr>",   desc = "CopilotChat - Improve wording" },
-			{ "<leader>ccc", "<cmd>CopilotChatConcise<cr>",   desc = "CopilotChat - Make text concise" },
+			-- Code related keys (default: <leader>cc)
+			{ "<leader>cce", "<cmd>CopilotChatExplain<cr>",       desc = "CopilotChat - Explain code" },
+			{ "<leader>cct", "<cmd>CopilotChatTests<cr>",         desc = "CopilotChat - Generate tests" },
+			{ "<leader>ccr", "<cmd>CopilotChatReview<cr>",        desc = "CopilotChat - Review code" },
+			{ "<leader>ccR", "<cmd>CopilotChatRefactor<cr>",      desc = "CopilotChat - Refactor code" },
+			{ "<leader>ccd", "<cmd>CopilotChatDocumentation<cr>", desc = "CopilotChat - Add documentation" },
+			{ "<leader>ccs", "<cmd>CopilotChatSimplify<cr>",      desc = "CopilotChat - Simplify code" },
+
+			-- Text related keys (default: <leader>ct)
+			{ "<leader>cts", "<cmd>CopilotChatSummarize<cr>",     desc = "CopilotChat - Summarize text" },
+			{ "<leader>ctS", "<cmd>CopilotChatSpelling<cr>",      desc = "CopilotChat - Correct spelling" },
+			{ "<leader>ctw", "<cmd>CopilotChatWording<cr>",       desc = "CopilotChat - Improve wording" },
+			{ "<leader>ctc", "<cmd>CopilotChatConcise<cr>",       desc = "CopilotChat - Make text concise" },
 		},
 	},
 	{
