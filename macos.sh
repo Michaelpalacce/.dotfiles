2ad14dd77a4ef3df8b6801abaaabdcd9655e6f77
Stefan Genov <stefantigro@gmail.com>
2024-01-21 18:41:49 +0200

    feat: Added macos.sh, for configuring some macos defaults

---
+++ macos.sh
@@ -0,0 +1,11 @@
+# MacOs is shit, but we can make it better...
+# This is a continuous effort
+
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+# Save screenshots to the desktop
+defaults write com.apple.screencapture location -string "$HOME/Desktop"
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
