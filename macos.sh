0b4cd6de31ce85c458e17a3dd9c7c691745ab5b8
Stefan Genov <stefantigro@gmail.com>
2024-01-21 19:19:51 +0200

    feat: Unshit macos

--- macos.sh
+++ macos.sh
@@ -1,11 +1,80 @@
 # MacOs is shit, but we can make it better...
 # This is a continuous effort
 
+
+# ------------------------ FINDER ----------------------------
+
 # Finder: show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
+# Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# ------------------------ SCREENSHOTS ----------------------------
+
 # Save screenshots to the desktop
 defaults write com.apple.screencapture location -string "$HOME/Desktop"
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
-# Display full POSIX path as Finder window title
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# ------------------------ KEYBOARD ----------------------------
+
+# Disable press-and-hold for keys in favor of key repeat.
+# Nobody cares about those special characters
+defaults write -g ApplePressAndHoldEnabled -bool false
+# Set a really fast key repeat.
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
+
+# ------------------------- OTHER ------------------------------
+
+# Reveal IP address, hostname, OS version, etc. when clicking the clock
+# in the login window
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
+
+# ------------------------- Trackpad ------------------------------
+
+# Trackpad: map bottom right corner to right-click
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
+
+# ------------------------- Dock ------------------------------
+
+# Change minimize/maximize window effect
+defaults write com.apple.dock mineffect -string "scale"
+# Enable highlight hover effect for the grid view of a stack (Dock)
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
+# Minimize windows into their application’s icon
+defaults write com.apple.dock minimize-to-application -bool true
+# Don’t animate opening applications from the Dock
+defaults write com.apple.dock launchanim -bool false
+# Speed up Mission Control animations
+defaults write com.apple.dock expose-animation-duration -float 0.1
+# Don’t group windows by application in Mission Control
+# (i.e. use the old Exposé behavior instead)
+defaults write com.apple.dock expose-group-by-app -bool false
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+# Remove the animation when hiding/showing the Dock
+defaults write com.apple.dock autohide-time-modifier -float 0
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+# Make Dock icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
+# Don’t show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
+
