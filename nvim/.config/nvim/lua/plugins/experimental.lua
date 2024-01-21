1303f07e71b014ba89e6596c5e797ad23915cbae
Stefan Genov <stefantigro@gmail.com>
2024-01-21 15:07:21 +0200

    feat: Trying things out

--- nvim/.config/nvim/lua/plugins/experimental.lua
+++ nvim/.config/nvim/lua/plugins/experimental.lua
@@ -1,2 +1,14 @@
 return {
+	-- {
+	-- 	'rasulomaroff/reactive.nvim',
+	-- 	config = function()
+	-- 		require('reactive').setup {
+	-- 			builtin = {
+	-- 				cursorline = true,
+	-- 				cursor = true,
+	-- 				modemsg = true
+	-- 			}
+	-- 		}
+	-- 	end
+	-- }
 }
