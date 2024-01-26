9113366bb2915c87db45901d3741aab92ecd1100
Stefan Genov <stefantigro@gmail.com>
2024-01-26 17:13:42 +0200

    feat: Add neotest, remove trouble

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -32,9 +32,39 @@ return {
 			-- Formatting
 			{ 'onsails/lspkind.nvim' },
 
+			-- Testing
+			{
+				"nvim-neotest/neotest",
+				dependencies = {
+					"nvim-lua/plenary.nvim",
+					"antoinemadec/FixCursorHold.nvim",
+					"nvim-treesitter/nvim-treesitter",
+					"nvim-neotest/neotest-go",
+				},
+				config = function()
+					-- get neotest namespace (api call creates or returns namespace)
+					local neotest_ns = vim.api.nvim_create_namespace("neotest")
+					vim.diagnostic.config({
+						virtual_text = {
+							format = function(diagnostic)
+								local message =
+									diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+								return message
+							end,
+						},
+					}, neotest_ns)
+
+					require("neotest").setup({
+						adapters = {
+							require("neotest-go"),
+						},
+					})
+				end
+			},
+
 			-- Langauges
 			-- LUA
-			{ "folke/neodev.nvim",                  opts = {} },
+			{ "folke/neodev.nvim", opts = {} },
 
 		},
 		config = function()
