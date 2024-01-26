05132797be0c928e26e43bc5051425c1510c55f5
Stefan Genov <stefantigro@gmail.com>
2024-01-26 17:38:09 +0200

    feat: NeoTest new remaps

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -36,6 +36,8 @@ return {
 			{
 				"nvim-neotest/neotest",
 				dependencies = {
+					'vim-test/vim-test',
+					'nvim-neotest/neotest-vim-test',
 					"nvim-lua/plenary.nvim",
 					"antoinemadec/FixCursorHold.nvim",
 					"nvim-treesitter/nvim-treesitter",
@@ -54,9 +56,27 @@ return {
 						},
 					}, neotest_ns)
 
+					vim.keymap.set("n", "<leader>Tr", "<cmd>lua require'neotest'.run.run()<CR>",
+						{ silent = true, desc = "Neotest: [R]un nearest" })
+
+					vim.keymap.set("n", "<leader>Tf", "<cmd>lua require'neotest'.run.run(vim.fn.expand('%'))<CR>",
+						{ silent = true, desc = "Neotest: Run [F]ile" })
+
+					vim.keymap.set("n", "<leader>Ts", "<cmd>lua require'neotest'.summary.toggle()<CR>",
+						{ silent = true, desc = "Neotest: Toggle [S]ummary" })
+
+					vim.keymap.set("n", "<leader>To", "<cmd>lua require('neotest').output.open({ enter = true })<CR>",
+						{ silent = true, desc = "Neotest: Toggle [O]utput" })
+
+					vim.keymap.set('n', '[n', '<cmd>lua require("neotest").jump.prev({ status = "failed" })<CR>',
+						{ noremap = true, silent = true, desc = "Neotest: Jump to [N]ext failed" })
+					vim.keymap.set('n', ']n', '<cmd>lua require("neotest").jump.next({ status = "failed" })<CR>',
+						{ noremap = true, silent = true, desc = "Neotest: Jump to [N]ext failed" })
+
 					require("neotest").setup({
 						adapters = {
 							require("neotest-go"),
+							require("neotest-vim-test")({ ignore_filetypes = { "go", "golang" } }),
 						},
 					})
 				end
