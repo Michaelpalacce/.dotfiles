f0ad0cf1a0395e3fd650079a60c575f84d76d904
Stefan Genov <stefantigro@gmail.com>
2024-01-28 15:13:28 +0200

    feat: Using native `lsp` capabilities
    nvim-ufo will automatically fold imports now

--- nvim/.config/nvim/lua/plugins/nvim-ufo.lua
+++ nvim/.config/nvim/lua/plugins/nvim-ufo.lua
@@ -13,11 +13,26 @@ return {
 			vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
 			vim.keymap.set('n', 'zr', require('ufo').openFoldsExceptKinds)
 			vim.keymap.set('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+			vim.keymap.set('n', 'zz', require('ufo').enableFold)
+
+			local capabilities = vim.lsp.protocol.make_client_capabilities()
+			capabilities.textDocument.foldingRange = {
+				dynamicRegistration = false,
+				lineFoldingOnly = true
+			}
+			local language_servers = require("lspconfig").util.available_servers() -- or list servers manually like {'gopls', 'clangd'}
+			for _, ls in ipairs(language_servers) do
+				require('lspconfig')[ls].setup({
+					capabilities = capabilities
+					-- you can add other fields for setting up lsp server in this table
+				})
+			end
 
 			require('ufo').setup({
-				provider_selector = function(bufnr, filetype, buftype)
-					return { 'treesitter', 'indent' }
-				end,
+				open_fold_hl_timeout = 150,
+				close_fold_kinds = {
+					'imports',
+				},
 			})
 		end
 	}
