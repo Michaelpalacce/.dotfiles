1e737c40bff565adaff090495800449a338303c5
Stefan Genov <stefantigro@gmail.com>
2024-01-28 14:23:54 +0200

    refactor(lsp): Removed `git` cmp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -21,7 +21,6 @@ return {
 			{ 'hrsh7th/cmp-nvim-lua' },
 			{ 'hrsh7th/cmp-cmdline' },
 			{ 'hrsh7th/cmp-nvim-lsp-signature-help' },
-			{ 'petertriho/cmp-git' },
 
 			-- Snippets
 			{ "L3MON4D3/LuaSnip" },
--- nvim/.config/nvim/lua/plugins/lsp/cmp.lua
+++ nvim/.config/nvim/lua/plugins/lsp/cmp.lua
@@ -105,13 +105,11 @@ cmp.setup({
 
 cmp.setup.filetype('gitcommit', {
 	sources = cmp.config.sources({
-		{ name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
 		{ name = 'path' },
 	}, {
 		{ name = 'buffer' },
 	})
 })
-require("cmp_git").setup()
 
 -- `/` cmdline setup.
 cmp.setup.cmdline('/', {
