7044c396760b917efbeec9e30f179615cec58589
Stefan Genov <stefantigro@gmail.com>
2024-01-31 01:35:42 +0200

    refactor: Moved colorsheme to themes

--- nvim/.config/nvim/lua/plugins/themes.lua
+++ nvim/.config/nvim/lua/plugins/themes.lua
@@ -1,3 +1,11 @@
 return {
-	{ "catppuccin/nvim", }, -- Theme
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme('catppuccin-macchiato')
+		end,
+	}, -- Theme
 }
