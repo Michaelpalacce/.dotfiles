e5be18fc5c15bb2c2341ca83e4283c353e327f94
Stefan Genov <sgenov94@gmail.com>
2024-04-06 21:47:28 +0300

    feat: Introducing codesnap plugin
    What a great plugin! Disabled the default watermark and added
    breadcrumbs, but overall solid

---
+++ nvim/.config/nvim/lua/plugins/codesnap.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"mistricky/codesnap.nvim",
+		build = "make",
+		config = function()
+			require("codesnap").setup({
+				mac_window_bar = true,
+				title = "CodeSnap.nvim",
+				code_font_family = "CaskaydiaCove Nerd Font",
+				watermark_font_family = "Pacifico",
+				watermark = "",
+				bg_theme = "default",
+				breadcrumbs_separator = "/",
+				has_breadcrumbs = true,
+			})
+		end,
+	}
+}
