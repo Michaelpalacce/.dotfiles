3c14cebde0dd572cfbce7c3881176d4796bf2c3c
Stefan Genov <sgenov94@gmail.com>
2024-04-10 10:32:28 +0300

    feat: Added installation of nix

--- scripts/extra.sh
+++ scripts/extra.sh
@@ -15,46 +15,59 @@ case "${unameOut}" in
     *)          machine="UNKNOWN:${unameOut}"
 esac
 
+# Install CARGO and rust
 if ! command_exists cargo; then
     curl https://sh.rustup.rs -sSf | sh -s -- -y
     source "$HOME/.cargo/env"
 fi
 
+# Install GOlang
 if ! command_exists go; then
     zsh <(curl -sL https://git.io/go-installer)
     source "$HOME/.zshrc"
     rm -rf go*.tar.gz
 
-    # if [[ "$machine" = "Linux" ]]; then
-    #     ln -sf $HOME/.dotfiles/sh/.zshenv.d/linux $HOME/.zshenv
-    # fi
 fi
 
+# Install fnm
 if ! command_exists fnm; then
     cargo install fnm
 fi
 
+# Install exa
 if ! command_exists exa; then
     cargo install exa
 fi
 
+# Install fzf
 if ! command_exists fzf; then
     git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
     ~/.fzf/install --completion --key-bindings --no-update-rc
 fi
 
+# Linux specifics
 if [[ "$machine" = "Linux" ]]; then
     if ! command_exists timer; then
         echo 'deb [trusted=yes] https://repo.caarlos0.dev/apt/ /' | sudo tee /etc/apt/sources.list.d/caarlos0.list
         apt update
         apt install timer
     fi
+
+    if ! command_exists nix; then
+        sh <(curl -L https://nixos.org/nix/install) --daemon --yes
+    fi
+
+    ln -sf $HOME/.dotfiles/sh/.zshenv.d/linux $HOME/.zshenv
 fi
 
-
+# Mac specifics
 if [[ "$machine" = "Mac" ]]; then
     if ! command_exists timer; then
         brew install caarlos0/tap/timer terminal-notifier 
+    fi
+
+    if ! command_exists nix; then
+        sh <(curl -L https://nixos.org/nix/install) --yes
     fi
 
     brew install gsed
