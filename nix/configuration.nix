1d4e40416eb87033110ce0fe2c5fe5dd8b3bc32e
Stefan Genov <sgenov94@gmail.com>
2024-04-16 01:39:53 +0300

    feat(nix): Unstable channel

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -101,6 +101,9 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # Enable the NixOS unstable channel
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
