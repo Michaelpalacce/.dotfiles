9d29da10f6d2d8405f6f7cc4630347211cb32e1e
Stefan Genov <sgenov94@gmail.com>
2024-04-16 02:02:25 +0300

    feat: Added a rebuild script courtese of noboilerplate
    https://github.com/0atman/noboilerplate/blob/main/scripts/38-nixos.md

---
+++ rebuild.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+pushd ~/dotfiles/nix/
+nvim configuration.nix
+alejandra . &>/dev/null
+git diff -U0 *.nix
+echo "NixOS Rebuilding..."
+sudo nixos-rebuild switch -I ~/.dotfiles/configuration.nix &>nixos-switch.log || (
+ cat nixos-switch.log | grep --color error && false)
+gen=$(nixos-rebuild list-generations | grep current)
+git commit -am "$gen"
+popd
