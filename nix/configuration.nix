5b15486112e8e90df636505fe6d2dfe444a14b86
Stefan Genov <sgenov94@gmail.com>
2024-04-16 09:07:51 +0300

    8 current   2024-04-16 09:07:47  23.11.6331.90055d5e616b  6.1.86                          *

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -174,6 +174,8 @@ in
     };
  };
 
+  environment.pathsToLink = [ "/share/zsh" ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
