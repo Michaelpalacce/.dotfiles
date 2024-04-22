152d77a66bd6f0ff5920a04610c4873c8ee17c38
Stefan Genov <stefantigro@gmail.com>
2024-04-23 01:18:42 +0300

    feat: Installation of home-manager to post
    Also improved detection

--- scripts/post.sh
+++ scripts/post.sh
@@ -2,6 +2,17 @@
 
 # Executes post installation and stow tasks
 
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
 
 # ------------------------ Home Manager ------------------------
-home-manager switch
+# This is done in the post since we want the configuration
+if command_exists home-manager; then
+    echo "Home Manager already installed"
+else
+    nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+    nix-channel --update
+    nix-shell '<home-manager>' -A install
+    home-manager switch
+fi
