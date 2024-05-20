f1fd09efd1472f556fef25199886912d57929b3e
Stefan Genov <sgenov94@gmail.com>
2024-05-20 17:24:57 +0300

    feat: added an autocommand that will highlight yanks

--- nvim/.config/nvim/plugin/autocmds.lua
+++ nvim/.config/nvim/plugin/autocmds.lua
@@ -3,3 +3,11 @@ vim.api.nvim_create_autocmd('BufReadPost', {
 	pattern = '*',
 	command = 'silent! normal! g`"zv'
 })
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+	desc = 'Highlight yanked text',
+	group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
+	callback = function()
+		vim.highlight.on_yank({ timeout = 50 })
+	end
+})
