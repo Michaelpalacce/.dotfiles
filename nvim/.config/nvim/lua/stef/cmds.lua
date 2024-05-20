5640942783ad4c899cd0326c8feab91a71a6b020
Stefan Genov <sgenov94@gmail.com>
2024-05-20 12:17:02 +0300

    feat(DelBuffers): Add command to delete all buffers
    Will delete all but the current buffer.

--- nvim/.config/nvim/lua/stef/cmds.lua
+++ nvim/.config/nvim/lua/stef/cmds.lua
@@ -2,3 +2,7 @@
 vim.api.nvim_create_user_command('WipeReg', function()
 	vim.cmd([[ for i in range(34,122) | silent! call setreg(nr2char(i), []) | endfor ]])
 end, { nargs = 0 })
+
+vim.api.nvim_create_user_command('DelBuffers', function()
+	vim.cmd([[ %bd|e# ]])
+end, { nargs = 0 })
