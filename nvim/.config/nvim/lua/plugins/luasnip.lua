e800487e4659027445cc51c30f7c14796002ec1d
Stefan Genov <sgenov94@gmail.com>
2024-05-21 09:28:33 +0300

    feat: luasnip gets loaded only for BufRead

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -6,7 +6,7 @@ return {
 		tag = "v2.0.0", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 		-- install jsregexp (optional!:).
 		build = "make install_jsregexp",
-		event = "VeryLazy",
+		event = "BufRead",
 		dependencies = {
 			-- { 'rafamadriz/friendly-snippets' },
 			{ 'saadparwaiz1/cmp_luasnip' },
