97fcae78a8e83ae7bb2593f770f6cadb3b032f39
Stefan Genov <sgenov94@gmail.com>
2024-05-24 16:19:54 +0300

    fix(home-manager): remove check in case of differences of version for home-manager and nix

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -14,6 +14,8 @@ in
   home.username = username;
   home.homeDirectory = homeDir;
 
+  home.enableNixpkgsReleaseCheck = false;
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
