c4c9c1c4908982fee84b61e01ab60206e2665cad
Stefan Genov <sgenov94@gmail.com>
2024-06-01 17:22:50 +0300

    feat(cheatsheet): Better lazy loading

--- nvim/.config/nvim/lua/plugins/cheatsheet.lua
+++ nvim/.config/nvim/lua/plugins/cheatsheet.lua
@@ -6,8 +6,8 @@ return {
 			-- { 'nvim-lua/popup.nvim' }, -- Not needed, install if any issues arise?
 			{ 'nvim-lua/plenary.nvim' },
 		},
-		config = function()
-			vim.keymap.set('n', '<leader>fc', '<cmd>Cheatsheet<CR>', { desc = 'Telescope: [F]ind In [C]heatsheet' })
-		end
+		keys = {
+			{ '<leader>fc', '<cmd>Cheatsheet<CR>', { desc = 'Telescope: [F]ind In [C]heatsheet' } }
+		},
 	}
 }
