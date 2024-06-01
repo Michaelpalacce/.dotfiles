1c48e6c1521ade9a5dc0f8f18c4f36f879cc0d0a
Stefan Genov <sgenov94@gmail.com>
2024-06-01 17:03:01 +0300

    feat: Add best plugin ever

--- nvim/.config/nvim/lua/plugins/experimental.lua
+++ nvim/.config/nvim/lua/plugins/experimental.lua
@@ -1,2 +1,11 @@
 return {
+	{
+		"zeioth/garbage-day.nvim",
+		dependencies = "neovim/nvim-lspconfig",
+		event = "VeryLazy",
+		opts = {
+			notifications = true,
+			grace_period = 30
+		}
+	},
 }
