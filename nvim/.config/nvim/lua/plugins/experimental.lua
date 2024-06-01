c491a683fceb132a86c9f36e4f96015fa090ee48
Stefan Genov <sgenov94@gmail.com>
2024-06-01 23:08:47 +0300

    feat(zen-mode)!: Remove zen-mode
    Don't like it after all

--- nvim/.config/nvim/lua/plugins/experimental.lua
+++ nvim/.config/nvim/lua/plugins/experimental.lua
@@ -14,13 +14,4 @@ return {
 		'NvChad/nvim-colorizer.lua',
 		config = true
 	},
-	-- Lua
-	{
-		"folke/zen-mode.nvim",
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		}
-	}
 }
