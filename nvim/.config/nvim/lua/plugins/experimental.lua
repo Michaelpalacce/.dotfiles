bc8fc1512485e97269435f86ebc6fbe10a88452f
Stefan Genov <stefantigro@gmail.com>
2024-06-07 23:41:42 +0300

    chore: Removed colorizer

--- nvim/.config/nvim/lua/plugins/experimental.lua
+++ nvim/.config/nvim/lua/plugins/experimental.lua
@@ -1,17 +1,2 @@
 return {
-	-- This plugin is used to stop inactive LSP servers after a certain period of time
-	-- {
-	-- 	"zeioth/garbage-day.nvim",
-	-- 	dependencies = "neovim/nvim-lspconfig",
-	-- 	event = "VeryLazy",
-	-- 	opts = {
-	-- 		notifications = true,
-	-- 		grace_period = 180
-	-- 	}
-	-- },
-	-- This plugin is used to colorize hex codes and color names in the buffer
-	{
-		'NvChad/nvim-colorizer.lua',
-		config = true
-	},
 }
