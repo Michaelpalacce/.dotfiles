0b255db6207eedf8d0d6b6d291f1f9cd298f3331
Stefan Genov <sgenov94@gmail.com>
2024-06-08 15:54:07 +0300

    feat: Added garbage day again with longer gracePeriod

--- nvim/.config/nvim/lua/plugins/experimental.lua
+++ nvim/.config/nvim/lua/plugins/experimental.lua
@@ -1,2 +1,12 @@
 return {
+	-- This plugin is used to stop inactive LSP servers after a certain period of time
+	{
+		"zeioth/garbage-day.nvim",
+		dependencies = "neovim/nvim-lspconfig",
+		event = "VeryLazy",
+		opts = {
+			notifications = true,
+			grace_period = 360
+		}
+	},
 }
