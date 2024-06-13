2d9724dbe3d6823acdfdd67e7f5a2cda4198654a
Stefan Genov <sgenov94@gmail.com>
2024-06-13 13:27:29 +0300

    reafctor: Moved flakes to home-manager

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -19,6 +19,12 @@ in
 
   home.enableNixpkgsReleaseCheck = false;
 
+  # Enable flake support
+    nix = {
+        package = pkgs.nix;
+        settings.experimental-features = [ "nix-command" "flakes" ];
+    };
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
