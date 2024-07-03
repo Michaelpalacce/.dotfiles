aa8cd9882707020090d622945b9dc4dd3e0568d1
Stefan Genov <sgenov94@gmail.com>
2024-07-03 13:43:49 +0300

    feat(cloak): Better matching for npmrc

--- nvim/.config/nvim/lua/plugins/experimental.lua
+++ nvim/.config/nvim/lua/plugins/experimental.lua
@@ -20,7 +20,7 @@ return {
 					{
 						-- Match any file starting with '.env'.
 						-- This can be a table to match multiple file patterns.
-						file_pattern = { '.env*', '.npmrc' },
+						file_pattern = { '.env*' },
 						-- Match an equals sign and any character after it.
 						-- This can also be a table of patterns to cloak,
 						-- example: cloak_pattern = { ':.+', '-.+' } for yaml files.
@@ -30,6 +30,12 @@ return {
 						-- where it doesn't cover the original text.
 						-- If left empty the legacy behavior of keeping the first character is retained.
 						replace = nil,
+					},
+
+					{
+						file_pattern = { '.npmrc*' },
+						cloak_pattern = { '(password)=.+' },
+						replace = "%1"
 					},
 
 					{
