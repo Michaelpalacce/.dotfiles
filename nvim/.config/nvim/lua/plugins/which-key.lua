89f421fa099f3d1e6464769ef8151ed20e6aacaf
Stefan Genov <sgenov94@gmail.com>
2024-07-14 13:50:16 +0300

    feat(which-key): Updated to v3

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -1,16 +1,28 @@
 return {
 	{
-		"folke/which-key.nvim", -- Shows a popup of key combinations
+		"folke/which-key.nvim",
 		event = "VeryLazy",
-		commit = "af4ded85542d40e190014c732fa051bdbf88be3d",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-		end,
 		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		}
-	},
+			delay = 0,
+			preset = "classic",
+			modes = {
+				n = true, -- Normal mode
+				i = true, -- Insert mode
+				x = false, -- Visual mode
+				s = true, -- Select mode
+				o = true, -- Operator pending mode
+				t = true, -- Terminal mode
+				c = true -- Command mode
+			},
+		},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	}
 }
