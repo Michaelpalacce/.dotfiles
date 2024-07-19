636038db6c1b250392b09b11d6cebdd5f254204d
Stefan Genov <stefantigro@gmail.com>
2024-07-19 18:12:38 +0300

    fix(harpoon): `append` was deprecated and replaced with `add`

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -12,7 +12,7 @@ return {
 			harpoon:setup()
 			-- REQUIRED
 
-			vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end,
+			vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end,
 				{ desc = "Harpoon: [A]dds file to harpoon" })
 			vim.keymap.set("n", "<leader>th", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
 				{ desc = "Harpoon: [T]oggles [H]arpoon menu" })
