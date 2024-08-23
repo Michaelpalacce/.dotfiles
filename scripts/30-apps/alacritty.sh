707db40b343dbbbbead655f09b003e9d9f322202
Stefan Genov <stefantigro@gmail.com>
2024-08-23 23:18:07 +0300

    refactor: Removed unnecessary code

--- scripts/30-apps/alacritty.sh
+++ scripts/30-apps/alacritty.sh
@@ -3,26 +3,6 @@
 # Alacritty cannot be installed with home-manager 
 # since it doesn't install well gui apps
  
-# Function to check if a command is available
-# https://stackoverflow.com/questions/592620/how-to-check-if-a-program-exists-from-a-bash-script
-command_exists() {
-  command -v "$1" >/dev/null 2>&1
-}
-
-# ANSI color codes
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No color
-
-# Function to print colored text
-print_color() {
-    local color="$1"
-    local message="$2"
-    echo -e "${color}${message}${NC}"
-}
-
 installAlacritty() {
     if command_exists brew; then
         brew install --cask alacritty
@@ -35,7 +15,6 @@ installAlacritty() {
         exit 1
     fi
 }
-
 
 if ! command_exists alacritty; then
     print_color "$GREEN" "alacritty not found, installing"
--- scripts/30-apps/zsh.sh
+++ scripts/30-apps/zsh.sh
@@ -2,38 +2,6 @@
 
 # Zsh installed outside of `home-manager` so the setup is in one place
 
-# Function to check if a command is available
-# https://stackoverflow.com/questions/592620/how-to-check-if-a-program-exists-from-a-bash-script
-command_exists() {
-  command -v "$1" >/dev/null 2>&1
-}
-
-# ANSI color codes
-GREEN='\033[0;32m'
-RED='\033[0;31m'
-YELLOW='\033[1;33m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No color
-
-# Function to print colored text
-print_color() {
-    local color="$1"
-    local message="$2"
-    echo -e "${color}${message}${NC}"
-}
-
-installOsSpecific() {
-    if command_exists apt-get; then
-        print_color "$GREEN" "Setting up"
-        sudo apt-get install -y "$1"
-    elif command_exists brew; then 
-        brew install "$1"
-    else
-        print_color "$RED" "Error: No package manager found"
-        exit 1
-    fi
-}
-
 # Check if zsh is installed
 if ! command_exists zsh; then
     installOsSpecific zsh
