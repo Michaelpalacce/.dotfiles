2d6cbd057c4381c1573e156c7ff88c6dcd053f10
Stefan Genov <sgenov94@gmail.com>
2024-10-03 15:19:54 +0300

    feat: Add `kubebuilder`

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -60,7 +60,7 @@ in
 
     # Kubernetes
     pkgs.k9s                pkgs.kubent         pkgs.kubectl    pkgs.velero         pkgs.unstable.fluxcd
-    pkgs.kubernetes-helm    pkgs.kind           pkgs.talosctl
+    pkgs.kubernetes-helm    pkgs.kind           pkgs.talosctl   pkgs.kubebuilder
 
     # Neovim
     pkgs.unstable.neovim    pkgs.ripgrep        pkgs.fd
