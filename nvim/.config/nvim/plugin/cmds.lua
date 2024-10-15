edd3522333adfcd2c3be6c8ea4e976d7aad2681c
Stefan Genov <stefantigro@gmail.com>
2024-10-15 15:51:04 +0300

    feat!: Removed changing of rel lines to non relative when entering insert, it's w/e

--- nvim/.config/nvim/plugin/cmds.lua
+++ nvim/.config/nvim/plugin/cmds.lua
@@ -6,7 +6,3 @@ end, { nargs = 0 })
 vim.api.nvim_create_user_command('DelBuffers', function()
 	vim.cmd([[ %bd!|e# ]])
 end, { nargs = 0 })
-
--- This will toggle the relative line numbers when entering insert mode
-vim.api.nvim_create_autocmd("InsertEnter", { command = [[set norelativenumber]] })
-vim.api.nvim_create_autocmd("InsertLeave", { command = [[set relativenumber]] })
