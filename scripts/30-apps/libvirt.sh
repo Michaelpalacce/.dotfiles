ac37850ffbcaaec2a9922c41e8cee21fc9ed5415
Stefan Genov <sgenov94@gmail.com>
2025-03-24 23:40:13 +0200

    feat: Added libvirt

---
+++ scripts/30-apps/libvirt.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# libvirtd is a system daemon and cannot be manged by home-manager.
+ 
+virtualization() {
+    if command_exists brew; then
+        exit 0
+    elif command_exists apt-get; then
+        sudo apt install -y qemu-kvm libvirt-daemon-system
+        sudo adduser $USER libvirt
+    else
+        print_color "$RED" "Error: No package manager found"
+        exit 1
+    fi
+}
+
+print_color "$GREEN" "installing kvm and libvirtd for emulation"
+virtualization
