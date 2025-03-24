a5aebb3158b0d60f366905882c01bcdab53d3936
Stefan Genov <stefantigro@gmail.com>
2025-03-24 11:46:51 +0200

    feat: Enhanced debugging

--- nvim/.config/nvim/lua/plugins/nvim-dap.lua
+++ nvim/.config/nvim/lua/plugins/nvim-dap.lua
@@ -5,7 +5,9 @@ return {
 		dependencies = {
 			'nvim-neotest/nvim-nio',
 			'rcarriga/nvim-dap-ui',
-			'leoluz/nvim-dap-go'
+			'leoluz/nvim-dap-go',
+			'jay-babu/mason-nvim-dap.nvim',
+			'theHamsta/nvim-dap-virtual-text'
 		},
 		-- Limit to go, cause that's all I use it for
 		ft = { "go", "gomod" },
