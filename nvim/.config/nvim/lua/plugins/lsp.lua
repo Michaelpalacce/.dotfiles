ba52b80753dfc71c720124e0b0195d5c34f8cb5b
Stefan Genov <sgenov94@gmail.com>
2025-04-09 21:55:53 +0300

    refactor!: Using the new LSP mappings

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -15,36 +15,32 @@ return {
 		},
 		config = function()
 			-- Remove https://gpanders.com/blog/whats-new-in-neovim-0-11/#more-default-mappings
-			pcall(vim.keymap.del, "n", "gra")
-			pcall(vim.keymap.del, "n", "gri")
-			pcall(vim.keymap.del, "n", "grn")
-			pcall(vim.keymap.del, "n", "grr")
 
 			-- lsp_attach is where you enable features that only work
 			-- if there is a language server active in the file
 			local lsp_attach = function(client, bufnr)
 				local opts = { buffer = bufnr }
 
-				vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-				vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-				vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-				vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-				vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-				vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-				vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+				-- vim.keymap.set('n', 'gri', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+				-- vim.keymap.set('n', 'grr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+				vim.keymap.set('n', 'grd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+
 				vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-				-- If you are searching for formatting, it's done by the plugin `conform.nvim`
 				vim.keymap.set({ 'n', 'v' }, '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 
+				vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+				vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+				vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+				-- If you are searching for formatting, it's done by the plugin `conform.nvim`
+
 				vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-				vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
-				vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
 			end
 
 			-- Diagnostics show different messages in case errors are found
 			vim.diagnostic.config({
-				virtual_text = { severity = { max = vim.diagnostic.severity.WARN, }, }, -- Virtual text at the end of the line
-				virtual_lines = { severity = { min = vim.diagnostic.severity.ERROR, }, }, -- line under the line
+				virtual_text = { severity = { max = vim.diagnostic.severity.ERROR, }, }, -- Virtual text at the end of the line
+				virtual_lines = false,
+				-- virtual_lines = { severity = { min = vim.diagnostic.severity.ERROR, }, }, -- line under the line
 				signs = {
 					text = {
 						[vim.diagnostic.severity.ERROR] = '✘',
