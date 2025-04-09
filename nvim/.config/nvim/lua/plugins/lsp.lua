491c3daa94eeb442e412f634fd893964358f34a6
Stefan Genov <stefantigro@gmail.com>
2025-04-09 11:27:00 +0300

    refactor: moved lsp plugins to own file

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,33 +1,4 @@
 return {
-	{
-		'williamboman/mason.nvim',
-		lazy = false,
-		config = true
-	},
-	{
-		'hrsh7th/nvim-cmp',
-		dependencies = {
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' },
-			{ 'hrsh7th/cmp-buffer' },
-			{ 'hrsh7th/cmp-path' },
-			{ 'hrsh7th/cmp-nvim-lsp' },
-			{ 'hrsh7th/cmp-nvim-lua' },
-			{ 'hrsh7th/cmp-cmdline' },
-			{ 'hrsh7th/cmp-nvim-lsp-signature-help' },
-
-			-- Icons
-			{ 'onsails/lspkind.nvim' },
-		},
-		event = 'InsertEnter',
-		config = function()
-			-- ##########################################################
-			-- Completion
-			-- ##########################################################
-
-			require "plugins.lsp.cmp" -- Contains all the completion configs
-		end
-	},
 	-- Using `nvim-lspconfig` to manage LSP configuration as they have amazing default support
 	{
 		'neovim/nvim-lspconfig',
---
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,26 @@
+return {
+	{
+		'hrsh7th/nvim-cmp',
+		dependencies = {
+			-- Autocompletion
+			{ 'hrsh7th/nvim-cmp' },
+			{ 'hrsh7th/cmp-buffer' },
+			{ 'hrsh7th/cmp-path' },
+			{ 'hrsh7th/cmp-nvim-lsp' },
+			{ 'hrsh7th/cmp-nvim-lua' },
+			{ 'hrsh7th/cmp-cmdline' },
+			{ 'hrsh7th/cmp-nvim-lsp-signature-help' },
+
+			-- Icons
+			{ 'onsails/lspkind.nvim' },
+		},
+		event = 'InsertEnter',
+		config = function()
+			-- ##########################################################
+			-- Completion
+			-- ##########################################################
+
+			require "plugins.lsp.cmp" -- Contains all the completion configs
+		end
+	},
+}
