659fad655d13fb16629801a9f36f7acdac0f0a9e
Stefan Genov <sgenov94@gmail.com>
2025-04-24 22:00:49 +0300

    feat(nix): Add terraform

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -73,6 +73,10 @@ in
     pkgs.unstable.nerd-fonts.jetbrains-mono
     pkgs.unstable.nerd-fonts.symbols-only
 
+    # IaC
+    pkgs.terraform
+
+
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
