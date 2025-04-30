dca14f14a07a4278a0e687dac4c89d9568b7e731
Stefan Genov <sgenov94@gmail.com>
2025-05-01 00:58:06 +0300

    feat(home-manager): using 24.11

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -32,7 +32,7 @@ in
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
 
   nixpkgs.config = {
     allowUnfree = true;
