e2c0d9b41e5376e7c619dee2a57fddb249982a26
Stefan Genov <sgenov94@gmail.com>
2025-05-03 21:11:34 +0300

    feat: better organziation of arch install

--- setup-arch.sh
+++ setup-arch.sh
@@ -25,17 +25,32 @@ else
   echo "yay is already installed."
 fi
 
-sudo pacman --noconfirm -Su vlc nfs-utils veracrypt waybar ttf-font-awesome \
-    ninja gcc wayland-protocols libjpeg-turbo libwebp libjxl pango cairo \
+sudo pacman --noconfirm -Su vlc nfs-utils veracrypt ninja gcc wayland-protocols libjpeg-turbo libwebp libjxl pango cairo \
     pkgconf cmake libglvnd meson gtk4 gobject-introspection libgirepository \
     gtk-doc rustup cargo
+
+yay --noconfirm -Su waybar ttf-font-awesome
 
 # Needed for rust builds (rustdesk for example)
 rustup default stable
 
-yay --noconfirm -Su mupdf catpuccin-gtk-theme-mocha nwg-look brightnessctl hypridle hyprlock hyprshot \
-    nautilus ttf-liberation steamcmd steam wine winetricks proton-cacyos cpio pkg-config \
-    gtkmm3 jsoncpp libsigc++ fmt chroon-date spdlog libgtk-3-dev alsa-utils hyprpicker python python-pip \
-    waypaper piper xorg-xev pipewire pipewire-pulse wireplumber pavucontrol swww
+yay --noconfirm -Su mupdf cpio pkg-config jsoncpp libsigc++ fmt chroon-date spdlog libgtk-3-dev alsa-utils python python-pip \
+    piper xorg-xev  
 
-yay --noconfirm -Su minecraft-launcher
+# look
+yay --noconfirm -Su swww catpuccin-gtk-theme-mocha nwg-look gtkmm3
+
+# audio
+yay --noconfirm -Su pipewire pipewire-pulse wireplumber pavucontrol
+
+# hyprland utils
+yay --noconfirm -Su hyprutils hyprgraphics qt5-wayland qt6-wayland xdg-desktop-portal-hyprland waypaper \
+    hyprpolkitagent hyprsysteminfo hypridle hyprlock hyprshot hyprpicker 
+
+# File Manager
+yay --noconfirm -Su nautilus nautilus-admin-gtk4 nautilus-image-converter nautilus-open-any-terminal
+
+# Gaming
+yay --noconfirm -Su minecraft-launcher \
+    ttf-liberation steamcmd steam wine winetricks proton-cacyos
+
