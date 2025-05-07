5df7c3b900ff6fae38be8a4664a13265af50af03
Stefan Genov <sgenov94@gmail.com>
2025-05-07 16:56:39 +0000

    fix(weather): Now no longer looks where teh fucky you are

--- waybar/.config/waybar/scripts/weather.sh
+++ waybar/.config/waybar/scripts/weather.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
-BSSIDS="$(nmcli device wifi list |
-    awk 'NR>1 {if ($1 != "*") {print $1}}' |
-    tr -d ":" |
-    tr "\n" ",")"
-
-LOC=""
-REQUEST_GEO="$(wget -qO - http://openwifi.su/api/v1/bssids/"$BSSIDS")"
-if [[ "$(jq ".count_results" <<< "$REQUEST_GEO")" -gt 0 ]] ; then
-    LAT="$(jq ".lat" <<< "$REQUEST_GEO")"
-    LON="$(jq ".lon" <<< "$REQUEST_GEO")"
-    LOC="$LAT,$LON"
-fi
+LOC="Plovdiv"
 
 text="$(curl -s "https://wttr.in/$LOC?format=1" | sed 's/ //g')"
 tooltip="$(curl -s "https://wttr.in/$LOC?0QT" |
@@ -19,6 +8,6 @@ tooltip="$(curl -s "https://wttr.in/$LOC?0QT" |
     sed 's/"/\\"/g')"
 
 if ! grep -q "Unknown location" <<< "$text"; then
-    echo "{\"text\": \"$text\", \"tooltip\": \"<tt>$tooltip</tt>\", \"class\": \"weather\"}"
+    echo "{\"text\": \"$text\", \"format\":\" {}\", \"tooltip\": \"<tt>$tooltip</tt>\"}"
 fi
 
