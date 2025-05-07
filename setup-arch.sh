911b47ad8f9f448079a7786c7fdea89d897325fc
Stefan Genov <sgenov94@gmail.com>
2025-05-07 22:42:47 +0300

    fix: yay install

--- setup-arch.sh
+++ setup-arch.sh
@@ -8,19 +8,21 @@ sudo pacman -S --needed base-devel git --noconfirm
 # Check if yay is already installed.
 if ! command -v yay &> /dev/null; then
   echo "Installing yay."
-  git clone https://aur.archlinux.org/yay.git
+  pushd $HOME 
+      git clone https://aur.archlinux.org/yay.git
 
-  pushd yay > /dev/null || {
-    echo "Failed to enter the yay directory.  Exiting."
-    exit 1
-  }
+      pushd yay > /dev/null || {
+        echo "Failed to enter the yay directory.  Exiting."
+        exit 1
+      }
 
-  makepkg -si --noconfirm
+      makepkg -si --noconfirm
 
-  popd > /dev/null || {
-    echo "Failed to return to the previous directory."
-    exit 1
-  }
+      popd > /dev/null || {
+        echo "Failed to return to the previous directory."
+        exit 1
+      }
+  popd
 else
   echo "yay is already installed."
 fi
