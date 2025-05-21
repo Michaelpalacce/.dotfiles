9c8964297c796d118a6a51bf81a27b8f1eba344f
Stefan Genov <sgenov94@gmail.com>
2025-05-21 09:47:35 +0300

    feat: Better floating
    Now will float and nicely resize to the middle of the screen.
    Also added a mainMod+C key to "center"

---
+++ hyprland/.config/hypr/scripts/better_floating.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+WINDOW_ADDRESS=$(hyprctl activewindow -j | jq -r '.address')
+IS_FLOATING=$(hyprctl clients -j | jq -r --arg addr "$WINDOW_ADDRESS" '.[] | select(.address == $addr) | .floating')
+
+if [ "$IS_FLOATING" = "true" ]; then
+    hyprctl dispatch settiled
+else
+    hyprctl dispatch setfloating
+    hyprctl dispatch resizeactive exact 50% 95%
+    hyprctl dispatch moveactive 0 5%
+    hyprctl dispatch centerwindow 1
+fi
