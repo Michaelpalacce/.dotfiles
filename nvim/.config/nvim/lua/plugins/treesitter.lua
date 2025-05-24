4e60c0cd852fd422a4093b66d04e422c1dc4fdd7
Stefan Genov <sgenov94@gmail.com>
2025-05-24 22:07:16 +0300

    chore(plugins): Bump to latest
    Treesitter needs to be eventually migrated to main branch

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,11 +2,12 @@ return {
 	{
 		'nvim-treesitter/nvim-treesitter', -- Provides syntax highlighting
 		build = ':TSUpdate',
-		event = "VeryLazy",
+		lazy = false,
+		branch = "master",
 		dependencies = {
 			'nvim-treesitter/nvim-treesitter-textobjects',
 		},
-		config = function()
+		config = {
 			require 'nvim-treesitter.configs'.setup {
 				-- A list of parser names, or "all"
 				ensure_installed = {},
@@ -43,77 +44,80 @@ return {
 				autotag = {
 					enable = true,
 				},
-
-				textobjects = {
-					-- swap = {
-					-- 	enable = true,
-					-- 	swap_next = {
-					-- 		["<leader>Ta"] = "@parameter.inner",
-					-- 	},
-					-- 	swap_previous = {
-					-- 		["<leader>TA"] = "@parameter.inner",
-					-- 	},
-					move = {
-						enable = true,
-						set_jumps = true, -- whether to set jumps in the jumplist
-						goto_next_start = {
-							["]m"] = "@function.outer",
-							["]]"] = { query = "@class.outer", desc = "Next class start" },
-						},
-						goto_next_end = {
-							["]M"] = "@function.outer",
-							["]["] = "@class.outer",
-						},
-						goto_previous_start = {
-							["[m"] = "@function.outer",
-							["[["] = "@class.outer",
-						},
-						goto_previous_end = {
-							["[M"] = "@function.outer",
-							["[]"] = "@class.outer",
-						},
+			}
+		},
+		{
+			'nvim-treesitter/nvim-treesitter-textobjects',
+			branch = "main",
+			opts = {
+				-- swap = {
+				-- 	enable = true,
+				-- 	swap_next = {
+				-- 		["<leader>Ta"] = "@parameter.inner",
+				-- 	},
+				-- 	swap_previous = {
+				-- 		["<leader>TA"] = "@parameter.inner",
+				-- 	},
+				move = {
+					enable = true,
+					set_jumps = true, -- whether to set jumps in the jumplist
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]]"] = { query = "@class.outer", desc = "Next class start" },
 					},
-					select = {
-						enable = true,
-
-						-- Automatically jump forward to textobj, similar to targets.vim
-						lookahead = true,
-
-						keymaps = {
-							-- You can use the capture groups defined in textobjects.scm
-							["af"] = "@function.outer",
-							["if"] = "@function.inner",
-						},
-						-- You can choose the select mode (default is charwise 'v')
-						--
-						-- Can also be a function which gets passed a table with the keys
-						-- * query_string: eg '@function.inner'
-						-- * method: eg 'v' or 'o'
-						-- and should return the mode ('v', 'V', or '<c-v>') or a table
-						-- mapping query_strings to modes.
-						selection_modes = {
-							['@parameter.outer'] = 'v', -- charwise
-							['@function.outer'] = 'V', -- linewise
-							['@class.outer'] = '<c-v>', -- blockwise
-						},
-						-- If you set this to `true` (default is `false`) then any textobject is
-						-- extended to include preceding or succeeding whitespace. Succeeding
-						-- whitespace has priority in order to act similarly to eg the built-in
-						-- `ap`.
-						--
-						-- Can also be a function which gets passed a table with the keys
-						-- * query_string: eg '@function.inner'
-						-- * selection_mode: eg 'v'
-						-- and should return true or false
-						include_surrounding_whitespace = true,
+					goto_next_end = {
+						["]M"] = "@function.outer",
+						["]["] = "@class.outer",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[["] = "@class.outer",
+					},
+					goto_previous_end = {
+						["[M"] = "@function.outer",
+						["[]"] = "@class.outer",
 					},
 				},
+				select = {
+					enable = true,
+
+					-- Automatically jump forward to textobj, similar to targets.vim
+					lookahead = true,
+
+					keymaps = {
+						-- You can use the capture groups defined in textobjects.scm
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+					},
+					-- You can choose the select mode (default is charwise 'v')
+					--
+					-- Can also be a function which gets passed a table with the keys
+					-- * query_string: eg '@function.inner'
+					-- * method: eg 'v' or 'o'
+					-- and should return the mode ('v', 'V', or '<c-v>') or a table
+					-- mapping query_strings to modes.
+					selection_modes = {
+						['@parameter.outer'] = 'v', -- charwise
+						['@function.outer'] = 'V', -- linewise
+						['@class.outer'] = '<c-v>', -- blockwise
+					},
+					-- If you set this to `true` (default is `false`) then any textobject is
+					-- extended to include preceding or succeeding whitespace. Succeeding
+					-- whitespace has priority in order to act similarly to eg the built-in
+					-- `ap`.
+					--
+					-- Can also be a function which gets passed a table with the keys
+					-- * query_string: eg '@function.inner'
+					-- * selection_mode: eg 'v'
+					-- and should return true or false
+					include_surrounding_whitespace = true,
+				},
 			}
-		end
-	},
-	-- {
-	-- 	'nvim-treesitter/nvim-treesitter-context', -- Provides a nice context of where you are
-	-- 	config = true,
-	-- 	event = "BufRead"
-	-- },
+		},
+		-- {
+		-- 	'nvim-treesitter/nvim-treesitter-context', -- Provides a nice context of where you are
+		-- 	config = true,
+		-- 	event = "BufRead"
+		-- },
+	}
 }
