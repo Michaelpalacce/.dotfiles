c1512df0e18ef4177373a93142c1505471c42341
Stefan Genov <sgenov94@gmail.com>
2025-05-29 12:08:17 +0300

    feat: Add nautilus extensions and re-added pseudo mode
    Pseudo mode is really usefull for wide screens

--- setup-arch.sh
+++ setup-arch.sh
@@ -63,8 +63,12 @@ yay --noconfirm -Su hyprutils hyprgraphics qt5-wayland qt6-wayland xdg-desktop-p
     hyprpolkitagent hyprsysteminfo hypridle hyprlock hyprshot hyprpicker lm_sensors
 
 # File Manager
-yay --noconfirm -Su nautilus nautilus-admin-gtk4 nautilus-image-converter nautilus-open-any-terminal ffmpegthumbnailer unzip \
-    transmission-gtk3
+yay --noconfirm -Su nautilus nautilus-admin-gtk4 nautilus-image-converter nautilus-open-any-terminalunzip \
+    transmission-gtk3 nautilus-annotations nautilus-bluetooth nautilus-checksums nautilus-hide nautilus-launch actions-for-nautilus-git \
+    folder-color-nautilus nautilus-metadata-editor nautilus-share seahorse-nautilus sushi file-roller
+
+# File Preview 
+yay --noconfirm -Su ffmpegthumbnailer gst-libav gst-plugins-ugly
 
 # Gaming
 yay --noconfirm -Su minecraft-launcher \
