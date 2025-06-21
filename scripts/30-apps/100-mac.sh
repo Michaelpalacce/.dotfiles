36903adcc97d8dea17c32702ccbb15ea159e17c2
Stefan Genov <sgenov94@gmail.com>
2025-06-21 14:30:10 +0300

    feat: Install alt-tab, remove wm

--- scripts/30-apps/100-mac.sh
+++ scripts/30-apps/100-mac.sh
@@ -5,8 +5,7 @@
 if command_exists brew; then
     print_color "$GREEN" "Installing mac apps"
     brew install --cask raycast # Better spotlight
-    # brew install --cask alt-tab # Linux-like alt-tab
-    brew install --cask nikitabobko/tap/aerospace # Tiling manager for MacOs
+    brew install --cask alt-tab # Linux-like alt-tab
 else
     print_color "$YELLOW" "Skipping for non-mac"
 fi
