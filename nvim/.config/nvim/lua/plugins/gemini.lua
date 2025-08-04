7f04a7576aee08f21c13785783724dc5d3870ba5
Stefan Genov <sgenov94@gmail.com>
2025-08-04 09:56:54 +0300

    feat: Added gemini
    I want to use it for explaining and easier chat interfacing.
    gemini.nvim doesn't seem like the best plugin, but we'll make do for now.

---
+++ nvim/.config/nvim/lua/plugins/gemini.lua
@@ -0,0 +1,14 @@
+return {
+	-- @NOTE: This requires you to have an env variable: GEMINI_API_KEY. I usually set those in .zsh_secrets or .zprofile
+	{
+		'kiddos/gemini.nvim',
+		opts = {
+			completion = {
+				enabled = false
+			},
+			instruction = {
+				menu_key = '<leader>ai'
+			}
+		},
+	}
+}
