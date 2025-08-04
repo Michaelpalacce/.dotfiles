2cf8f1ac490bb5ce0f5094bf2b90654aee65d286
Stefan Genov <sgenov94@gmail.com>
2025-08-04 22:44:08 +0300

    feat: Added aerospace

--- scripts/30-apps/100-mac.sh
+++ scripts/30-apps/100-mac.sh
@@ -6,7 +6,7 @@ if command_exists brew; then
     print_color "$GREEN" "Installing mac apps"
     brew install --cask raycast # Better spotlight
     # brew install --cask alt-tab # Linux-like alt-tab
--    brew install --cask nikitabobko/tap/aerospace # Tiling manager for MacOs
+    brew install --cask nikitabobko/tap/aerospace # Tiling manager for MacOs
 else
     print_color "$YELLOW" "Skipping for non-mac"
 fi
