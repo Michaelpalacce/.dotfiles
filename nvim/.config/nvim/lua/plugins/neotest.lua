6c6e3e326571ef6100e6b63c3f1f74930108cf11
Stefan Genov <sgenov94@gmail.com>
2025-08-04 21:51:29 +0300

    feat: Moved configuration in correct places
    Keeping it simpler

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -13,11 +13,41 @@ return {
 		-- Limit to go, cause that's all I use it for
 		ft = { "go", "gomod" },
 		config = function()
-			-- ##########################################################
-			-- Testing
-			-- ##########################################################
+			-- get neotest namespace (api call creates or returns namespace)
+			local neotest_ns = vim.api.nvim_create_namespace("neotest")
+			vim.diagnostic.config({
+				virtual_text = {
+					format = function(diagnostic)
+						local message =
+							diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+						return message
+					end,
+				},
+			}, neotest_ns)
 
-			require "plugins.lsp.testing"
+			vim.keymap.set("n", "<leader>Tr", "<cmd>lua require'neotest'.run.run()<CR>",
+				{ silent = true, desc = "Neotest: [R]un nearest" })
+
+			vim.keymap.set("n", "<leader>Tf", "<cmd>lua require'neotest'.run.run(vim.fn.expand('%'))<CR>",
+				{ silent = true, desc = "Neotest: Run [F]ile" })
+
+			vim.keymap.set("n", "<leader>Ts", "<cmd>lua require'neotest'.summary.toggle()<CR>",
+				{ silent = true, desc = "Neotest: Toggle [S]ummary" })
+
+			vim.keymap.set("n", "<leader>To", "<cmd>lua require('neotest').output.open({ enter = true })<CR>",
+				{ silent = true, desc = "Neotest: Toggle [O]utput" })
+
+			vim.keymap.set('n', '[n', '<cmd>lua require("neotest").jump.prev({ status = "failed" })<CR>',
+				{ noremap = true, silent = true, desc = "Neotest: Jump to [N]ext failed" })
+			vim.keymap.set('n', ']n', '<cmd>lua require("neotest").jump.next({ status = "failed" })<CR>',
+				{ noremap = true, silent = true, desc = "Neotest: Jump to [N]ext failed" })
+
+			require("neotest").setup({
+				adapters = {
+					require("neotest-go"),
+					require("neotest-vim-test")({ ignore_filetypes = { "go", "golang" } }),
+				},
+			})
 		end
 	},
 }
