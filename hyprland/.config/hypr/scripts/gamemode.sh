4ed57a885232342bd1bd9d6dde5237fe92728eb5
Stefan Genov <sgenov94@gmail.com>
2025-08-06 23:38:55 +0300

    feat: Lock is now suspend directly

--- hyprland/.config/hypr/scripts/gamemode.sh
+++ hyprland/.config/hypr/scripts/gamemode.sh
@@ -7,7 +7,6 @@ if [ "$HYPRGAMEMODE" = 1 ] ; then
         keyword decoration:blur:enabled 0;\
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
-        keyword general:border_size 1;\
         keyword decoration:rounding 0"
     exit
 fi
