b6b19dff57b9f46991250fcc585636d63b384d15
Stefan Genov <sgenov94@gmail.com>
2025-08-17 13:15:32 +0300

    feat: Install `fd`

--- arch/30-dev/50-development.sh
+++ arch/30-dev/50-development.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Dev
-yay --noconfirm -S tmux zsh stow aws-cli-v2 nvim fnm terraform ansible ripgrep sops age fzf
+yay --noconfirm -S tmux zsh stow aws-cli-v2 nvim fnm terraform ansible ripgrep sops age fzf fd
 
 
 if [ "$MINIMAL" == false ]; then
