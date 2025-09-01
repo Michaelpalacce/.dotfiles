eeec8ac8e2991c1538a5c0a6489203d827007274
Stefan Genov <sgenov94@gmail.com>
2025-09-01 08:21:13 +0300

    fix: Remove bluetooth module

--- setup/arch/10-gui/50-file-manager.sh
+++ setup/arch/10-gui/50-file-manager.sh
@@ -3,7 +3,7 @@
 if [ "$MINIMAL" == false ]; then
     # File Manager
     yay --noconfirm -S --needed nautilus nautilus-admin-gtk4 nautilus-image-converter nautilus-open-any-terminalunzip \
-        transmission-gtk3 nautilus-annotations nautilus-bluetooth nautilus-checksums nautilus-hide nautilus-launch actions-for-nautilus-git \
+        transmission-gtk3 nautilus-annotations nautilus-checksums nautilus-hide nautilus-launch actions-for-nautilus-git \
         folder-color-nautilus nautilus-metadata-editor nautilus-share seahorse-nautilus sushi file-roller
 
     # File Preview 
