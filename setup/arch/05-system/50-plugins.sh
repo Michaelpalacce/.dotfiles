846732e4c2a40dd17cdcad54d6041ad71a0bd094
Stefan Genov <sgenov94@gmail.com>
2025-09-12 11:27:56 +0300

    feat: Ask if hyprbars are wanted

--- setup/arch/05-system/50-plugins.sh
+++ setup/arch/05-system/50-plugins.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 if [ "$MINIMAL" == false ]; then
-    echo "YOU WILL BE PROMPTED FOR PASSWORD A FEW TIMES!"
-    hyprpm update
-    yes | hyprpm add https://github.com/hyprwm/hyprland-plugins
-    hyprpm enable hyprbars
+    read -p "Do you want hyprbars?" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo "YOU WILL BE PROMPTED FOR PASSWORD A FEW TIMES!"
+        hyprpm update
+        yes | hyprpm add https://github.com/hyprwm/hyprland-plugins
+        hyprpm enable hyprbars
+    else
+        hyprpm disable hyprbars
+    fi
 fi
