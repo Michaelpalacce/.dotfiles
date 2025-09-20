97d1914d9b724155c69bcbd95f0b7e46e4517103
Stefan Genov <sgenov94@gmail.com>
2025-09-20 16:38:49 +0300

    feat!: Migrate to wildfire instead of treesitter

--- ROADMAP.md
+++ ROADMAP.md
@@ -1,3 +1,2 @@
 # Roadmap
 
-- [ ] Tree-sitter migration to main branch after incremental_selection is supported in one way or another
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,20 +3,10 @@ return {
 		'nvim-treesitter/nvim-treesitter', -- Provides syntax highlighting
 		build = ':TSUpdate',
 		lazy = false,
-		branch = "master",
-		dependencies = {
-			'nvim-treesitter/nvim-treesitter-textobjects',
-		},
+		branch = "main",
 		config = {
 			require 'nvim-treesitter.configs'.setup {
 				-- A list of parser names, or "all"
-				ensure_installed = {},
-				ignore_install = {},
-				modules = {},
-
-				-- Install parsers synchronously (only applied to `ensure_installed`)
-				sync_install = false,
-
 				-- Automatically install missing parsers when entering buffer
 				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
 				auto_install = true,
@@ -33,83 +23,29 @@ return {
 					-- Instead of true it can also be a list of languages
 					additional_vim_regex_highlighting = true,
 				},
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<C-e>", -- set to `false` to disable one of the mappings
-						node_incremental = "<C-e>",
-						node_decremental = "<C-x>",
-					},
-				},
 				autotag = {
 					enable = true,
 				},
 			}
 		},
 		{
-			'nvim-treesitter/nvim-treesitter-textobjects',
-			branch = "main",
-			opts = {
-				move = {
-					enable = true,
-					set_jumps = true, -- whether to set jumps in the jumplist
-					goto_next_start = {
-						["]m"] = "@function.outer",
-						["]]"] = { query = "@class.outer", desc = "Next class start" },
-					},
-					goto_next_end = {
-						["]M"] = "@function.outer",
-						["]["] = "@class.outer",
-					},
-					goto_previous_start = {
-						["[m"] = "@function.outer",
-						["[["] = "@class.outer",
-					},
-					goto_previous_end = {
-						["[M"] = "@function.outer",
-						["[]"] = "@class.outer",
-					},
-				},
-				select = {
-					enable = true,
-
-					-- Automatically jump forward to textobj, similar to targets.vim
-					lookahead = true,
-
+			"sustech-data/wildfire.nvim",
+			event = "VeryLazy",
+			dependencies = { "nvim-treesitter/nvim-treesitter" },
+			config = function()
+				require("wildfire").setup {
 					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
+						init_selection = "<C-e>", -- set to `false` to disable one of the mappings
+						node_incremental = "<C-e>",
+						node_decremental = "<C-x>",
 					},
-					-- You can choose the select mode (default is charwise 'v')
-					--
-					-- Can also be a function which gets passed a table with the keys
-					-- * query_string: eg '@function.inner'
-					-- * method: eg 'v' or 'o'
-					-- and should return the mode ('v', 'V', or '<c-v>') or a table
-					-- mapping query_strings to modes.
-					selection_modes = {
-						['@parameter.outer'] = 'v', -- charwise
-						['@function.outer'] = 'V', -- linewise
-						['@class.outer'] = '<c-v>', -- blockwise
-					},
-					-- If you set this to `true` (default is `false`) then any textobject is
-					-- extended to include preceding or succeeding whitespace. Succeeding
-					-- whitespace has priority in order to act similarly to eg the built-in
-					-- `ap`.
-					--
-					-- Can also be a function which gets passed a table with the keys
-					-- * query_string: eg '@function.inner'
-					-- * selection_mode: eg 'v'
-					-- and should return true or false
-					include_surrounding_whitespace = true,
-				},
-			}
+				}
+			end,
 		},
-		-- {
-		-- 	'nvim-treesitter/nvim-treesitter-context', -- Provides a nice context of where you are
-		-- 	config = true,
-		-- 	event = "BufRead"
-		-- },
+		{
+			'nvim-treesitter/nvim-treesitter-context', -- Provides a nice context of where you are
+			config = true,
+			event = "BufRead"
+		},
 	}
 }
