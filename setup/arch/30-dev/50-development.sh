c7365c1446438f16316643250e58af8c7773faef
Stefan Genov <sgenov94@gmail.com>
2025-09-23 18:02:23 +0300

    feat: Add `chart-testing` or `ct`

--- setup/arch/30-dev/50-development.sh
+++ setup/arch/30-dev/50-development.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Dev
-yay --noconfirm -S --needed aws-cli-v2 nvim fnm terraform ansible ripgrep fzf fd direnv vault-bin golangci-lint github-cli
+yay --noconfirm -S --needed aws-cli-v2 nvim fnm terraform ansible ripgrep fzf fd direnv vault-bin golangci-lint github-cli chart-testing
 
 if [ "$MINIMAL" == false ]; then
     # Postman GUI app
