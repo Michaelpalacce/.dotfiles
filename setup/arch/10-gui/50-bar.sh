c5aa097cbd6465c420742ed33a18286bb6084cce
Stefan Genov <sgenov94@gmail.com>
2025-09-23 14:48:21 +0300

    feat: removed python-gpustat it is not used
    Was used for hyprbar, but I don't need that

--- setup/arch/10-gui/50-bar.sh
+++ setup/arch/10-gui/50-bar.sh
@@ -5,5 +5,5 @@ if [ "$MINIMAL" == false ]; then
     # Explicit dependencies
     yay --noconfirm -S --needed ags-hyprpanel-git wireplumber libgtop bleuz bluez-utils networkmanager dart-sass wl-clipboard upower gvfs gtksourceview3 libsoup3
     # Optional
-    yay --noconfirm -S --needed python python-gpustat brightnessctl pywal pacman-contrib power-profiles-daemon grimblast wf-recorder hyprpicker hyprsunset btop matugen swww
+    yay --noconfirm -S --needed python brightnessctl pywal pacman-contrib power-profiles-daemon grimblast wf-recorder hyprpicker hyprsunset btop matugen swww
 fi
