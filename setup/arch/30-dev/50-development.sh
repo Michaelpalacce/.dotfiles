0c42ab3759d26a02a06d30da4fb88055da9d822a
Stefan Genov <sgenov94@gmail.com>
2025-09-30 08:46:08 +0300

    feat: add `yq`

--- setup/arch/30-dev/50-development.sh
+++ setup/arch/30-dev/50-development.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # Dev
-yay --noconfirm -S --needed aws-cli-v2 nvim fnm terraform ansible ripgrep fzf fd direnv vault-bin golangci-lint github-cli chart-testing
+yay --noconfirm -S --needed aws-cli-v2 nvim fnm terraform ansible vault-bin golangci-lint github-cli chart-testing
+
+# Utils
+yay --noconfirm -S --needed ripgrep fzf fd direnv yq jq
 
 if [ "$MINIMAL" == false ]; then
     # Postman GUI app
