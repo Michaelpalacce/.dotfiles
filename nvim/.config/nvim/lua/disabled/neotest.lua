20eb02378a2aba60fe0a717d277e8132ff641d09
Stefan Genov <sgenov94@gmail.com>
2025-10-02 16:25:38 +0300

    chore(plugins): Bump to latest

--- nvim/.config/nvim/lua/disabled/neotest.lua
+++ nvim/.config/nvim/lua/disabled/neotest.lua
@@ -5,6 +5,7 @@ return {
 		dependencies = {
 			'vim-test/vim-test',
 			'nvim-neotest/neotest-vim-test',
+			'nvim-neotest/nvim-nio',
 			"nvim-lua/plenary.nvim",
 			"antoinemadec/FixCursorHold.nvim",
 			"nvim-treesitter/nvim-treesitter",
