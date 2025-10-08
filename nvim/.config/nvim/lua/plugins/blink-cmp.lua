d7559a0beacd486bfd0560ad52b48b481175d846
Stefan Genov <sgenov94@gmail.com>
2025-10-08 14:25:37 +0300

    chore(plugins): Bump to latest

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -34,13 +34,14 @@ return {
 				['<C-p>'] = { 'select_prev', 'fallback_to_mappings' },
 				['<C-n>'] = { 'select_next', 'fallback_to_mappings' },
 
+				['<Tab>'] = { 'select_next' },
+				['<S-Tab>'] = { 'select_prev' },
+
 				['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
 				['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
 
 				['<CR>'] = { "accept", 'fallback' },
 
-				['<Tab>'] = { 'select_next' },
-				['<S-Tab>'] = { 'select_prev' },
 
 				['<C-k>'] = { 'show_signature', 'hide_signature', 'fallback' },
 
