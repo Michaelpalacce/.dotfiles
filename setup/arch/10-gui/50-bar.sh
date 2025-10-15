5b504b1dcc5d68ac662aa19b6fdae40bbd7861e5
Stefan Genov <sgenov94@gmail.com>
2025-10-15 21:11:45 +0300

    feat: Removed unneeded hyprpanel deps

--- setup/arch/10-gui/50-bar.sh
+++ setup/arch/10-gui/50-bar.sh
@@ -5,9 +5,9 @@ if [ "$MINIMAL" == false ]; then
     # Waybar
     yay --noconfirm -S --needed waybar swaync
 
-    # Explicit dependencies
-    yay -S --needed aylurs-gtk-shell-git wireplumber libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl swww python upower pacman-contrib power-profiles-daemon gvfs gtksourceview3 libsoup3 grimblast-git wf-recorder-git hyprpicker matugen-bin python-gpustat hyprsunset-git
-
-    # Hyprpanel
-    yay -S --needed ags-hyprpanel-git
+    # # Explicit dependencies for Hyprpanel
+    # yay -S --needed aylurs-gtk-shell-git wireplumber libgtop bluez bluez-utils btop networkmanager dart-sass wl-clipboard brightnessctl swww python upower pacman-contrib power-profiles-daemon gvfs gtksourceview3 libsoup3 grimblast-git wf-recorder-git hyprpicker matugen-bin python-gpustat hyprsunset-git
+    #
+    # # Hyprpanel
+    # yay -S --needed ags-hyprpanel-git
 fi
