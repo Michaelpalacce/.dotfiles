9cb767e3bea90c8d435051610a14a8faea94f30a
Stefan Genov <sgenov94@gmail.com>
2025-11-21 22:53:03 +0000

    fix: Formatting fixes

--- setup/arch/20-network/50-netowrking.sh
+++ setup/arch/20-network/50-netowrking.sh
@@ -3,5 +3,4 @@
 # Networking... bind has tools like nslookup
 yay --noconfirm -S --needed networkmanager bind dnsmasq
 
-if [ "$MINIMAL" == false ]; then
-fi
+
--- setup/arch/40-apps/50-gaming.sh
+++ setup/arch/40-apps/50-gaming.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$MINIMAL" == false && "$GAMING" == true ]; then
+if [[ "$MINIMAL" == false && "$GAMING" == true ]]; then
     # Gaming
     yay --noconfirm -S --needed minecraft-launcher mesa nwjs-sdk-bin vulkantools vulkan-icd-loader lib32-vulkan-icd-loader \
         ttf-liberation steamcmd steam wine winetricks vesktop-bin lutris lutris-wine-meta bottles wine-mono wine-gecko
