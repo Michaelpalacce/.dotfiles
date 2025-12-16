e068b6870b88f9a77e362ff3737d17c76cfb2a62
Stefan Genov <sgenov94@gmail.com>
2025-12-16 09:18:53 +0200

    feat(nautilus): Removed unuseds stuff

--- setup/arch/10-gui/50-file-manager.sh
+++ setup/arch/10-gui/50-file-manager.sh
@@ -2,10 +2,13 @@
 
 if [[ "$MINIMAL" == false ]]; then
     # File Manager
-    yay --noconfirm -S --needed nautilus nautilus-admin-gtk4 nautilus-image-converter nautilus-open-any-terminal unzip \
-        transmission-gtk3 nautilus-annotations nautilus-checksums nautilus-hide nautilus-launch actions-for-nautilus-git \
-        folder-color-nautilus nautilus-metadata-editor nautilus-share seahorse-nautilus sushi file-roller
+    yay --noconfirm -S --needed nautilus nautilus-open-any-terminal nautilus-launch actions-for-nautilus-git sushi
 
     # File Preview 
     yay --noconfirm -S --needed ffmpegthumbnailer gst-libav gst-plugins-ugly
+
+    # File Utils
+    # transmission-gtk3 for torrenting
+    # unzup and file-roller for archiving
+    yay --noconfirm -S --needed transmission-gtk3 unzip file-roller
 fi
