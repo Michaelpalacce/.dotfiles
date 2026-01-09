d1428863b0455deb70837066738f387cb8785040
Stefan Genov <sgenov94@gmail.com>
2026-01-09 13:43:39 +0200

    feat: wallpaper

--- setup/arch/10-gui/50-visuals-and-gui.sh
+++ setup/arch/10-gui/50-visuals-and-gui.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 if [[ "$MINIMAL" == false ]]; then
-    yay --noconfirm -S --needed catppuccin-gtk-theme-mocha nwg-look gtkmm3 qt6-svg qt6-declarative qt5-quickcontrols2 waycorner brightnessctl swww light
+    yay --noconfirm -S --needed catppuccin-gtk-theme-mocha nwg-look gtkmm3 qt6-svg qt6-declarative qt5-quickcontrols2 waycorner brightnessctl light
 
     # hyprland utils
     yay --noconfirm -S --needed hyprutils hyprgraphics qt5-wayland qt6-wayland xdg-desktop-portal-hyprland \
         hyprpolkitagent hyprsysteminfo hypridle hyprlock hyprshot hyprpicker lm_sensors hyprviz-bin
 
     # Wallpaper
-    yay --noconfirm -S --needed waypaper
+    yay --noconfirm -S --needed waypaper swww mpvpaper
 fi
