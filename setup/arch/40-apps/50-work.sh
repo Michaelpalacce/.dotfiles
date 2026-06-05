3fc29c843c506c6ac7e3aeeef89c1d9807b047d6
Stefan Genov <sgenov94@gmail.com>
2026-06-05 11:05:26 +0300

    feat: Remove `pomodoro`

--- setup/arch/40-apps/50-work.sh
+++ setup/arch/40-apps/50-work.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-yay --noconfirm -S --needed gnome-pomodoro
+# yay --noconfirm -S --needed gnome-pomodoro
 
 if [[ "$MINIMAL" == false ]]; then
     yay --noconfirm -S --needed slack-desktop
